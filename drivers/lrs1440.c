72f0986fc3fa16fdad2a93f0874035651aa80bc0
Stefan Ritt <stefan.ritt@psi.ch>
1999-01-14 15:57:54 +0000

    Adapted to new "info" structure
    SVN revision: 144

--- drivers/lrs1440.c
+++ drivers/lrs1440.c
@@ -6,6 +6,9 @@
   Contents:     LeCroy LRS 1440 High Voltage Device Driver
 
   $Log$
+  Revision 1.3  1999/01/14 15:57:54  midas
+  Adapted to new "info" structure
+
   Revision 1.2  1998/10/12 12:18:57  midas
   Added Log tag in header
 
@@ -19,164 +22,180 @@
 
 /*---- globals -----------------------------------------------------*/
 
-#define MAX_DEVICES 10
+typedef struct {
+  char rs232_port[NAME_LENGTH];
+  int  address;
+  int  polarity[16];
+} LRS1440_SETTINGS;
 
-static char   rs232_port[MAX_DEVICES][NAME_LENGTH];
-static INT    lrs1440_address[MAX_DEVICES];
-static INT    *polarity[MAX_DEVICES];
+#define LRS1440_SETTINGS_STR "\
+RS232 Port = STRING : [32] com1\n\
+Address = INT : 1\n\
+Polarity = INT[16] :\n\
+[0] -1\n\
+[1] -1\n\
+[2] -1\n\
+[3] -1\n\
+[4] -1\n\
+[5] -1\n\
+[6] -1\n\
+[7] -1\n\
+[8] -1\n\
+[9] -1\n\
+[10] -1\n\
+[11] -1\n\
+[12] -1\n\
+[13] -1\n\
+[14] -1\n\
+"
 
-static int    hdev[MAX_DEVICES];  /* device handle for RS232 device */
+typedef struct {
+  LRS1440_SETTINGS settings;
+  int num_channels;
+  int hdev;                            /* device handle for RS232 device */
+} LRS1440_INFO;
 
 /*---- device driver routines --------------------------------------*/
 
-void lrs1440_switch(index)
+void lrs1440_switch(LRS1440_INFO *info)
 {
-static INT last_index=-1;
+static INT last_address=-1;
 char str[80];
 INT  status;
 
-  if (index != last_index)
+  if (info->settings.address != last_address)
     {
-    rs232_puts(hdev[index], "M16\r\n");
-    rs232_waitfor(hdev[index], "M16\r\n", str, 80, 5);
-    sprintf(str, "M%02d\r\n", lrs1440_address[index]);
-    rs232_puts(hdev[index], str);
-    status = rs232_waitfor(hdev[index], "responding\r\n", str, 80, 5);
+    rs232_puts(info->hdev, "M16\r\n");
+    rs232_waitfor(info->hdev, "M16\r\n", str, 80, 5);
+    sprintf(str, "M%02d\r\n", info->settings.address);
+    rs232_puts(info->hdev, str);
+    status = rs232_waitfor(info->hdev, "responding\r\n", str, 80, 5);
     if (!status)
       {
       cm_msg(MERROR, "lrs1440_init", 
-        "LRS1440 #%d doesn't respond. Check power and RS232 connection.", 
-        lrs1440_address[index]);
+        "LRS1440 adr %d doesn't respond. Check power and RS232 connection.", 
+        info->settings.address);
       return;
       }
 
-    last_index = index;
+    last_address = info->settings.address;
     }
 }
 
 /*------------------------------------------------------------------*/
 
-INT lrs1440_init(HNDLE hKeyRoot, INT index, INT channels)
+INT lrs1440_init(HNDLE hKey, void **pinfo, INT channels)
 {
-int   status, size, i, n_cards;
-char  str[256];
-HNDLE hDB;
+int          status, size;
+char         str[256];
+HNDLE        hDB;
+LRS1440_INFO *info;
+
+  /* allocate info structure */
+  info = calloc(1, sizeof(LRS1440_INFO));
+  *pinfo = info;
 
   cm_get_experiment_database(&hDB, NULL);
 
-  /* read LRS1440 settings */
-  strcpy(rs232_port[index], "COM2");
-  size = NAME_LENGTH;
-  db_get_value(hDB, hKeyRoot, "RS232 Port", rs232_port[index], &size, TID_STRING);
-  
-  lrs1440_address[index] = index+1;
-  size = sizeof(INT);
-  db_get_value(hDB, hKeyRoot, "Address", &lrs1440_address[index], &size, TID_INT);
-  
-  n_cards = (channels-1)/16+1;
-  polarity[index] = malloc(sizeof(INT)*n_cards);
-  for (i=0 ; i<n_cards ; i++)
-    polarity[index][i] = -1; /* default polarity is negative */
-  db_merge_data(hDB, hKeyRoot, "Polarity", polarity[index], sizeof(INT)*n_cards, n_cards, TID_INT);
+  /* create LRS1440 settings record */
+  status = db_create_record(hDB, hKey, "", LRS1440_SETTINGS_STR);
+  if (status != DB_SUCCESS)
+    return FE_ERR_ODB;
+
+  size = sizeof(info->settings);
+  db_get_record(hDB, hKey, &info->settings, &size, 0);
+
+  info->num_channels = channels;
 
   /* initialize RS232 port */
-  hdev[index] = 0;
-  for (i=0 ; i<index ; i++)
-    if (strcmp(rs232_port[i], rs232_port[index]) == 0)
-      {
-      hdev[index] = hdev[i];
-      break;
-      }
-
-  if (hdev[index] == 0)
-    hdev[index] = rs232_init(rs232_port[index], 9600, 'N', 8, 1, 0);
+  info->hdev = rs232_init(info->settings.rs232_port, 9600, 'N', 8, 1, 0);
 
   rs232_debug(FALSE);
 
   /* check if module is living  */
-  rs232_puts(hdev[index], "M16\r\n");
-  status = rs232_waitfor(hdev[index], "M16\r\n", str, 80, 2);
+  rs232_puts(info->hdev, "M16\r\n");
+  status = rs232_waitfor(info->hdev, "M16\r\n", str, 80, 2);
   if (!status)
     {
-    cm_msg(MERROR, "lrs1440_init", "LRS1440 adr %d doesn't respond. Check power and RS232 connection.", lrs1440_address[index]);
+    cm_msg(MERROR, "lrs1440_init", "LRS1440 adr %d doesn't respond. Check power and RS232 connection.", info->settings.address);
     return FE_ERR_HW;
     }
 
-  sprintf(str, "M%02d\r\n", lrs1440_address[index]);
-  rs232_puts(hdev[index], str);
-  status = rs232_waitfor(hdev[index], "responding\r\n", str, 80, 3);
+  sprintf(str, "M%02d\r\n", info->settings.address);
+  rs232_puts(info->hdev, str);
+  status = rs232_waitfor(info->hdev, "responding\r\n", str, 80, 3);
   if (!status)
     {
-    cm_msg(MERROR, "lrs1440_init", "LRS1440 adr %d doesn't respond. Check power and RS232 connection.", lrs1440_address[index]);
+    cm_msg(MERROR, "lrs1440_init", "LRS1440 adr %d doesn't respond. Check power and RS232 connection.", info->settings.address);
     return FE_ERR_HW;
     }
 
   /* check if HV enabled */
-  rs232_puts(hdev[index], "ST\r");
-  rs232_waitfor(hdev[index], "abled", str, 80, 3);
+  rs232_puts(info->hdev, "ST\r");
+  rs232_waitfor(info->hdev, "abled", str, 80, 3);
 
   if (str[strlen(str) - 6] == 's')
     {
     /* read rest of status */
-    rs232_gets(hdev[index], str, 80, 0);
+    rs232_gets(info->hdev, str, 80, 0);
 
-    cm_msg(MERROR, "lrs1440_init", "LeCroy1440 #%d: HV disabled by front panel button", index);
+    cm_msg(MERROR, "lrs1440_init", "LeCroy1440 adr %d: HV disabled by front panel button", info->settings.address);
 //    return FE_ERR_HW;
     }
 
   /* read rest of status */
-  rs232_gets(hdev[index], str, 80, 0);
+  rs232_gets(info->hdev, str, 80, 0);
 
   /* turn on HV main switch */
-  rs232_puts(hdev[index], "ON\r");
-  rs232_waitfor(hdev[index], "Turn on\r\n", str, 80, 5);
+  rs232_puts(info->hdev, "ON\r");
+  rs232_waitfor(info->hdev, "Turn on\r\n", str, 80, 5);
 
-  rs232_puts(hdev[index], "M16\r\n");
+  rs232_puts(info->hdev, "M16\r\n");
 
   return FE_SUCCESS;
 }
 
 /*----------------------------------------------------------------------------*/
 
-INT lrs1440_exit(INT index)
+INT lrs1440_exit(LRS1440_INFO *info)
 {
-  rs232_exit(hdev[index]);
+  rs232_exit(info->hdev);
   
-  free(polarity[index]);
+  free(info);
 
   return FE_SUCCESS;
 }
 
 /*----------------------------------------------------------------------------*/
 
-INT lrs1440_set(INT index, INT channel, float value)
+INT lrs1440_set(LRS1440_INFO *info, INT channel, float value)
 {
 char  str[80];
 
-  lrs1440_switch(index);
+  lrs1440_switch(info);
 
-  sprintf(str, "W%04.0f C%02d\r", polarity[index][channel/16]*value, channel);
+  sprintf(str, "W%04.0f C%02d\r", info->settings.polarity[channel/16]*value, channel);
 
-  rs232_puts(hdev[index], str);
-  rs232_waitfor(hdev[index], "\r\n\r\n", str, 80, 3);
+  rs232_puts(info->hdev, str);
+  rs232_waitfor(info->hdev, "\r\n\r\n", str, 80, 3);
 
   return FE_SUCCESS;
 }
 
 /*----------------------------------------------------------------------------*/
 
-INT lrs1440_set_all(INT index, INT channels, float value)
+INT lrs1440_set_all(LRS1440_INFO *info, INT channels, float value)
 {
 char  str[80];
 INT   i;
 
-  lrs1440_switch(index);
+  lrs1440_switch(info);
 
   for (i=0 ; i<(channels-1)/16+1 ; i++)
     {
-    sprintf(str, "W%04.0f C%d DO16\r", polarity[index][i/16]*value, i*16);
-    rs232_puts(hdev[index], str);
-    rs232_waitfor(hdev[index], "\r\n\r\n", str, 80, 3);
+    sprintf(str, "W%04.0f C%d DO16\r", info->settings.polarity[i/16]*value, i*16);
+    rs232_puts(info->hdev, str);
+    rs232_waitfor(info->hdev, "\r\n\r\n", str, 80, 3);
     }
 
   return FE_SUCCESS;
@@ -184,17 +203,17 @@ INT   i;
 
 /*----------------------------------------------------------------------------*/
 
-INT lrs1440_get(INT index, INT channel, float *pvalue)
+INT lrs1440_get(LRS1440_INFO *info, INT channel, float *pvalue)
 {
 int   value;
 char  str[256];
 
-  lrs1440_switch(index);
+  lrs1440_switch(info);
 
   sprintf(str, "R V C%02d\r", channel);
-  rs232_puts(hdev[index], str);
-  rs232_waitfor(hdev[index], "Act ", str, 80, 1);
-  rs232_waitfor(hdev[index], "\r\n\r\n", str, 80, 1);
+  rs232_puts(info->hdev, str);
+  rs232_waitfor(info->hdev, "Act ", str, 80, 1);
+  rs232_waitfor(info->hdev, "\r\n\r\n", str, 80, 1);
   sscanf(str+1, "%d", &value);
 
   *pvalue = (float) value;
@@ -204,49 +223,49 @@ char  str[256];
 
 /*----------------------------------------------------------------------------*/
 
-INT lrs1440_get_all(INT index, INT channels, float *array)
+INT lrs1440_get_all(LRS1440_INFO *info, INT channels, float *array)
 {
 int   value, i, j;
 char  str[256];
 
-  lrs1440_switch(index);
+  lrs1440_switch(info);
 
   sprintf(str, "R V F C0 DO%d\r", channels);
-  rs232_puts(hdev[index], str);
+  rs232_puts(info->hdev, str);
 
   for (i=0 ; i <= (channels-1)/8 ; i++)
     {
-    rs232_waitfor(hdev[index], "Act ", str, 80, 5);
-    rs232_waitfor(hdev[index], "\r\n", str, 80, 2);
+    rs232_waitfor(info->hdev, "Act ", str, 80, 5);
+    rs232_waitfor(info->hdev, "\r\n", str, 80, 2);
     for (j=0 ; j<8 && i*8+j < channels; j++)
       {
       sscanf(str + 1 + j*6, "%d", &value);
       array[i*8 + j] = (float) value;
       }
     }
-  rs232_waitfor(hdev[index], "\r\n", str, 80, 2);
+  rs232_waitfor(info->hdev, "\r\n", str, 80, 2);
 
   return FE_SUCCESS;
 }
 
 /*----------------------------------------------------------------------------*/
 
-INT lrs1440_set_current_limit(INT index, float limit)
+INT lrs1440_set_current_limit(LRS1440_INFO *info, float limit)
 {
 char str[256];
 
-  lrs1440_switch(index);
+  lrs1440_switch(info);
 
   if (limit > 255)
     limit = 255.f;
 
   sprintf(str, "LI %1.0f\r", limit);
-  rs232_puts(hdev[index], str);
-  rs232_waitfor(hdev[index], "\r\n\r\n", str, 80, 1);
+  rs232_puts(info->hdev, str);
+  rs232_waitfor(info->hdev, "\r\n\r\n", str, 80, 1);
 
   sprintf(str, "LI %1.0f\r", -limit);
-  rs232_puts(hdev[index], str);
-  rs232_waitfor(hdev[index], "\r\n\r\n", str, 80, 1);
+  rs232_puts(info->hdev, str);
+  rs232_waitfor(info->hdev, "\r\n\r\n", str, 80, 1);
 
   return FE_SUCCESS;
 }
@@ -257,8 +276,9 @@ INT lrs1440(INT cmd, ...)
 {
 va_list argptr;
 HNDLE   hKey;
-INT     index, channel, status;
+INT     channel, status;
 float   value, *pvalue;
+void    *info;
 
   va_start(argptr, cmd);
   status = FE_SUCCESS;
@@ -267,42 +287,42 @@ float   value, *pvalue;
     {
     case CMD_INIT:
       hKey = va_arg(argptr, HNDLE);
-      index = va_arg(argptr, INT);
+      info = va_arg(argptr, void *);
       channel = va_arg(argptr, INT);
-      status = lrs1440_init(hKey, index, channel);
+      status = lrs1440_init(hKey, info, channel);
       break;
 
     case CMD_EXIT:
-      index = va_arg(argptr, INT);
-      status = lrs1440_exit(index);
+      info = va_arg(argptr, void *);
+      status = lrs1440_exit(info);
       break;
 
     case CMD_SET:
-      index = va_arg(argptr, INT);
+      info = va_arg(argptr, void *);
       channel = va_arg(argptr, INT);
       value   = (float) va_arg(argptr, double);
-      status = lrs1440_set(index, channel, value);
+      status = lrs1440_set(info, channel, value);
       break;
 
     case CMD_SET_ALL:
-      index = va_arg(argptr, INT);
+      info = va_arg(argptr, void *);
       channel = va_arg(argptr, INT);
       value   = (float) va_arg(argptr, double);
-      status = lrs1440_set_all(index, channel, value);
+      status = lrs1440_set_all(info, channel, value);
       break;
 
     case CMD_GET:
-      index = va_arg(argptr, INT);
+      info = va_arg(argptr, void *);
       channel = va_arg(argptr, INT);
       pvalue  = va_arg(argptr, float*);
-      status = lrs1440_get(index, channel, pvalue);
+      status = lrs1440_get(info, channel, pvalue);
       break;
     
     case CMD_GET_ALL:
-      index = va_arg(argptr, INT);
+      info = va_arg(argptr, void *);
       channel = va_arg(argptr, INT);
       pvalue  = va_arg(argptr, float*);
-      status = lrs1440_get_all(index, channel, pvalue);
+      status = lrs1440_get_all(info, channel, pvalue);
       break;
 
     /* current measurements not supported in LRS1440 */
@@ -314,11 +334,11 @@ float   value, *pvalue;
       break;
       
     case CMD_SET_CURRENT_LIMIT:
-      index = va_arg(argptr, INT);
+      info = va_arg(argptr, void *);
       channel = va_arg(argptr, INT);
       value = (float) va_arg(argptr, double);
       if (channel == 0) /* one one limit for all channels */
-        status = lrs1440_set_current_limit(index, value);
+        status = lrs1440_set_current_limit(info, value);
       else
         status = FE_SUCCESS;
       break;
