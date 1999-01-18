866a54fe65b311a1e5f48c06e960f8908dc0f455
Pierre Amaudruz <amaudruz@triumf.ca>
1999-01-18 17:34:36 +0000

    - cleanup definitions and structures for ybos - Correct prototype for ybos
    SVN revision: 153

--- include/ybos.h
+++ include/ybos.h
@@ -5,15 +5,15 @@
 
   Contents:     Declarations for ybos data format
 
-
   Revision history
   ------------------------------------------------------------------
   date        by    modification
   ---------   ---   ------------------------------------------------
-  15-JAN-97   PAA   created
-  12-MAY-97   SR    changed RING_SIZE from NET_BUFFER_SIZE to
-                    MAX_EVENT_SIZE
-
+*  $Log$
+*  Revision 1.6  1999/01/18 17:34:36  pierre
+*  - cleanup definitions and structures for ybos
+*  - Correct prototype for ybos
+*
 \********************************************************************/
 
 #ifdef OS_WINNT
@@ -32,50 +32,22 @@
 #endif
 
 /*---- General parameters -----------------------------*/
-/* All parameters given in 4bytes word. */
-
-#define YBOS_BLK_SIZE        8190
-#define YBOS_NB_BLK             4
+#define YBOS_PHYREC_SIZE        8190  /* I*4 */
 #define YBOS_HEADER_LENGTH      4
-#define RING_BLK_SIZE        YBOS_BLK_SIZE + (YBOS_NB_BLK-1)*(YBOS_BLK_SIZE-YBOS_HEADER_LENGTH)
-#define RING_SIZE            RING_BLK_SIZE*4 + MAX_EVENT_SIZE   /* + max event size (midas)*/
-#define YBOS_SUCCESS            1
-#define YBOS_FAIL_MALLOC_RING  -1
-#define YBOS_FAIL_OPEN         -2
-#define YBOS_FAIL_FLUSH        -3
-#define YBOS_WRONG_BANK_TYPE   -4
-#define YBOS_BANK_NOT_FOUND    -5
+#define YBOS_BUFFER_SIZE        3*(YBOS_PHYREC_SIZE<<2) + MAX_EVENT_SIZE + 128           /* in BYTES */
 
-/* rdlog global definitions */
-#define NLINE                    8    /* number of elements for display routine */
-#define RDLOG_SUCCESS            1
-#define RDLOG_DONE               2
-#define RDLOG_EOR                3
-#define RDLOG_COMPLETE           4
-#define RDLOG_INCOMPLETE         5
+#define YB_BANKLIST_MAX        32     /* maximum number of banks to be found 
+                                         by the xbk_list() */
+#define YB_STRING_BANKLIST_MAX          YB_BANKLIST_MAX * 4
+                                       /* to be used for xbk_list() */
 
-/* Global error */
-#define RDLOG_FAIL_OPEN         -1
-#define RDLOG_FAIL_READING      -2
-#define RDLOG_EOF_REACHED       -3
-#define RDLOG_DEV_CLOSED        -4
-#define RDLOG_BLOCK_TOO_BIG     -5
-#define RDLOG_ALREADY_OPEN      -6
-#define RDLOG_UNKNOWN_FMT       -7
-#define RDLOG_FAIL_CLOSE        -8
-#define RDLOG_NOMORE_SLOT       -9
-
-/* YBOS swapping flags */
-#define YBOS_EVENT_SWAPPED       1
-#define YBOS_EVENT_NOT_SWAPPED   0
-#define YBOS_SWAP_ERROR         -1
-
-/* YBOS Bank types */
-#define I2_BKTYPE       1
-#define A1_BKTYPE       2
-#define I4_BKTYPE       3
-#define F4_BKTYPE       4
-#define I1_BKTYPE       8
+#define YB_SUCCESS               1
+#define YB_EVENT_NOT_SWAPPED     2
+#define YB_DONE                  2
+#define YB_WRONG_BANK_TYPE    -100
+#define YB_BANK_NOT_FOUND     -101
+#define YB_SWAP_ERROR         -102
+#define YB_NOMORE_SLOT        -103
 
 /* record header content */
 #define H_BLOCK_SIZE   0     /* YBOS */
@@ -83,44 +55,19 @@
 #define H_HEAD_LEN     2     /* YBOS */
 #define H_START        3     /* YBOS */
 
-/* rdlog global definitions */
-#define NLINE                    8    /* number of elements for display routine */
-#define RDLOG_SUCCESS            1
-#define RDLOG_DONE               2
-#define RDLOG_EOR                3
-
-/* Display mode options */
-#define DSP_RAW    1
-#define DSP_BANK   2
-#define DSP_DEC    1
-#define DSP_HEX    2
-#define DSP_ASC    3
-
-/* file option */
-#define NO_RECOVER  -1
-#define NO_RUN       0
-#define ADD_RUN      1
-
 /* Display parameters */
 #define D_RECORD       1
 #define D_HEADER       2
-#define D_EVENT        3
-#define D_EVTLEN       4
+#define D_EVTLEN       3
 
-/* rlog global definitions */
-#define MAX_DLOG_UNIT            8    /* max data logger channels */
-#define MAX_DLOG_FMT             1    /* max data logger data format type */
-#define MAX_EVT_SIZE         0xFFFF   /* max event size */
-#define MAX_BLOCK_SIZE       0xFFFF   /* max block size */
-#define MAX_DLOG_NAME_SIZE     128    /* max data logger name size */
-#define MAX_DLOG_LABEL_SIZE     32    /* max data logger label size */
-#define MAX_FILE_PATH          128
-#define DLOG_DISK                1    /* type of device (disk file) */
-#define DLOG_TAPE                2    /* type of device (tape file) */
-#define VT                       1    /* type of device for error log */
+/* File fragmentation */
+#define YB_COMPLETE       1
+#define YB_INCOMPLETE     2
+#define YB_NO_RECOVER    -1
+#define YB_NO_RUN         0
+#define YB_ADD_RUN        1
 
 /*---- Macros for YBOS ------------------------*/
-
 /* word swap (I4=I21I22 -> I4=I22I21 */
 #define SWAP_D2WORD(_d2w) {\
   WORD _tmp2;                                    \
@@ -129,11 +76,87 @@
   *(((WORD *)(_d2w))+1) = _tmp2;                 \
 }
 
+/* the s for the following macros */
+#ifdef CHAOS_EVID_FMT
+extern chaos;
+#endif
+
+/*                                          s=TRUE>CHAOS        s=FALSE>NORMAL */
+#define YBOS_EVID_EVENT_ID(e)      *(e-2) == 6 ? *((WORD *)(e)+3)  : *((WORD *)(e)+1)
+#define YBOS_EVID_TRIGGER_MASK(e)  *(e-2) == 6 ? *((WORD *)(e)+2)  : *((WORD *)(e)+0)
+#define YBOS_EVID_SERIAL(e)        *(e-2) == 6 ? *((DWORD *)(e)+2) : *((DWORD *)(e)+1)
+#define YBOS_EVID_TIME(e)          *(e-2) == 6 ? *((DWORD *)(e)+3) : *((DWORD *)(e)+2)
+#define YBOS_EVID_RUN_NUMBER(e)    *(e-2) == 6 ? *((DWORD *)(e)+4) : *((DWORD *)(e)+3)
+#define YBOS_EVID_EVENT_NB(e)      *(e-2) == 6 ? *((DWORD *)(e)+0) : *((DWORD *)(e)+1)
+
+/*                     pevt Evt# id/msk serial run# */
+#define YBOS_EVID_BANK(__a, __b, __c,   __d,   __e) {\
+      DWORD * pbuf;\
+      ybk_create(__a, "EVID", I4_BKTYPE, &pbuf);\
+      *(pbuf)++ = (DWORD)__b;\
+      *(pbuf)++ = (DWORD)__c;\
+      *(pbuf)++ = (DWORD)__d;\
+      *(pbuf)++ = (DWORD)ss_millitime();\
+      *(pbuf)++ = (DWORD)__e;\
+      ybk_close(__a, pbuf);\
+        }
+
+/*                      pevt Evt# id/msk serial run# */
+#define MIDAS_EVID_BANK(__a, __b, __c,   __d,   __e) {\
+      DWORD * pbuf;\
+      bk_create(__a, "EVID", TID_DWORD, &pbuf);\
+      *(pbuf)++ = (DWORD)__b;\
+      *(pbuf)++ = (DWORD)__c;\
+      *(pbuf)++ = (DWORD)__d;\
+      *(pbuf)++ = (DWORD)ss_millitime();\
+      *(pbuf)++ = (DWORD)__e;\
+      bk_close(__a, pbuf);\
+        }
+
 /*---- data structures for YBOS file format ------------------------*/
+/* YBOS Bank types */
+#define I2_BKTYPE       1
+#define A1_BKTYPE       2
+#define I4_BKTYPE       3
+#define F4_BKTYPE       4
+#define I1_BKTYPE       8
+#define MAX_BKTYPE      I1_BKTYPE+1
+
+/* YBOS logger channel ALL in 4BYTES! */
+typedef struct {
+  DWORD *ptop;         /* pointer to top of YBOS buffer */
+  DWORD *pbuf;         /* current data pointer for writing to buffer */
+  DWORD *pwrt;         /* current data pointer for writing to device */
+  DWORD *pbot;         /* bottom of the physical record */
+  DWORD *pend;         /* end of the buffer */
+  DWORD  reco;         /* offset to first logical record 4BYTES */
+  DWORD  recn;         /* current YBOS physical record number */
+} YBOS_INFO;
+
+/* YBOS Physical record header */
+typedef struct {
+  DWORD  rec_size;          /* LPR Length of Physical Record (exclusive) */
+  DWORD  header_length;     /* LPH Length of Physical Header (inclusive) */
+  DWORD  rec_num;           /* NPR Physical Record Number (start 0) */
+  DWORD  offset;            /* LRD Offset to 1st Logical Record 
+                                   (=LPH for 1st Logical record) */
+} YBOS_PHYSREC_HEADER;
+
+/* Bank header */
+typedef struct {
+  DWORD  name;              /* bank name 4ASCII */
+  DWORD  number;            /* bank number (=1) */
+  DWORD  index;             /* index within bank (=0) */
+  DWORD  length;            /* bank length (I*4, exclusive ) */
+  DWORD  type;              /* bank type refer to above */
+} YBOS_BANK_HEADER;
+
 
 /* YBOS FILE parameters */
-#define  MAX_FRAG_SIZE   2000          /* max event size for fragmented file (bytes) */
-#define  MAX_YM_FILE        8          /* max concurrent file handling */
+#define MAX_FILE_PATH    128
+#define MAX_FRAG_SIZE   2000          /* max event size for fragmented file (bytes) */
+#define MAX_YM_FILE        8          /* max concurrent file handling */
+#define NLINE              8          /* number of elements for display routine */
 
 /* YBOS control file header (private structure) */
 typedef struct{
@@ -149,7 +172,7 @@ typedef struct{
 
 /* YBOS path file header (private structure) */
 typedef struct{
-  char path[128];
+  char path[MAX_FILE_PATH];
 } YM_PFILE;
 
 /* YBOS file replay handler (for multiple file entries) */
@@ -161,83 +184,52 @@ typedef struct {
   char path[MAX_FILE_PATH];
 } R_YM_FILE;
 
-/*---- data structures for YBOS format -----------------------------*/
+/* Display mode options */
+#define DSP_RAW    1
+#define DSP_BANK   2
 
-/* Midas YBOS logger channel */
-typedef struct {
-  DWORD *top_ptr;         /* top ring_buffer data pointer */
-  DWORD *bot_ptr;         /* bottom ring_buffer data pointer */
-  DWORD *rd_cur_ptr;      /* current read data pointer (from buffer to tape) */
-  DWORD *wr_cur_ptr;      /* current write (from event to buffer) */
-  DWORD *blk_end_ptr;     /* end of Ybos data block pointer */
-  DWORD  event_offset;     /* offset to first logical record in bytes */
-  DWORD  cur_block;        /* current YBOS block number */
-} YBOS_INFO;
-
-/* YBOS Physical record header */
-typedef struct {
-  DWORD  block_size;        /* LPR Length of Physical Record (exclusive) */
-  DWORD  header_length;     /* LPH Length of Physical Header (inclusive) */
-  DWORD  block_num;         /* NPR Physical Record Number (start 0) */
-  DWORD  offset;            /* LRD Offset to 1st Logical Record (=LPH for 1st logical record */
-                           /*     in first physical record (== LPH)*/
-} YBOS_PHYSREC_HEADER;
-
-/* YBOS BANK record
-   The MONO-TYPE YBOS bank should have the following structure
-   I*4 Bank name            ... (4 ascii)
-   I*4 Bank number             0
-   I*4 Index in Bank           1
-   I*4 Length of Bank       ... (exclusive from this point in I*4)
-   I*4 Bank type            ... (see #define)
-   */
-typedef struct {
-  DWORD  name;              /* bank name 4ASCII */
-  DWORD  number;            /* bank number (=1) */
-  DWORD  index;             /* index within bank (=0) */
-  DWORD  length;            /* bank length (I*4, exclusive ) */
-  DWORD  type;              /* bank type refer to above */
-} YBOS_BANK_HEADER;
+/* Display format */
+#define DSP_DEC    1
+#define DSP_HEX    2
+#define DSP_ASC    3
 
 /*---- function declarations ---------------------------------------*/
+INT   EXPRT yb_file_recompose(void * pevt, INT fmt, char * svpath, INT file_mode);
+INT   EXPRT feodb_file_dump (EQUIPMENT * eqp, char * eqpname,char * pevent, INT run_number, char *path);
 
-void  EXPRT display_any_bank(void * pbk, INT data_fmt, INT dsp_mode, INT dsp_fmt);
-void  EXPRT display_any_event(void * pevt, INT data_fmt, INT dsp_mode, INT dsp_fmt);
-void  EXPRT display_any_physrec(INT data_fmt, INT dsp_fmt);
-void  EXPRT display_any_physhead(INT data_fmt, INT dsp_fmt);
-void  EXPRT display_any_evtlen(INT data_fmt, INT dsp_fmt);
+void  EXPRT yb_any_bank_display(void * pbk, INT fmt, INT dsp_mode, INT dsp_fmt);
+void  EXPRT yb_any_event_display(void * pevt, INT data_fmt, INT dsp_mode, INT dsp_fmt);
+INT   EXPRT yb_any_all_info_display (INT what);
+INT   EXPRT yb_any_physrec_display(INT data_fmt, INT dsp_fmt);
 
-INT   EXPRT any_magta_write( INT logH, INT fmt, char *pwt, INT nbytes);
-INT   EXPRT get_any_physrec(INT data_fmt);
-INT   EXPRT get_any_event(INT data_fmt, INT bl, void *prevent);
-INT   EXPRT skip_any_physrec(INT data_fmt, INT bl);
-INT   EXPRT swap_any_event(INT data_fmt, void * prevent);
+INT   EXPRT yb_any_physrec_skip(INT data_fmt, INT bl);
+INT   EXPRT yb_any_physrec_get (INT data_fmt, void ** prec, DWORD * psize);
+INT   EXPRT yb_any_file_rclose (INT data_fmt);
+INT   EXPRT yb_any_file_ropen(char * infile, INT data_fmt);
+INT   EXPRT yb_any_file_wopen (INT type, INT data_fmt, char * filename, INT * hDev);
+INT   EXPRT yb_any_file_wclose (INT handle, INT type, INT data_fmt);
+INT   EXPRT yb_any_log_write (INT handle, INT data_fmt, INT type, void * prec, DWORD nbytes);
+INT   EXPRT yb_any_event_swap (INT data_fmt, void * pevent);
+INT   EXPRT yb_any_event_get (INT data_fmt, INT bl, void ** pevent, DWORD * psize);
 
-INT   EXPRT open_any_datafile (INT data_fmt, char * datafile);
-INT   EXPRT close_any_datafile (INT data_fmt);
-
-INT   EXPRT open_any_logfile (INT log_type, INT data_fmt, char * device_name, HNDLE * hDev);
-INT   EXPRT close_any_logfile (INT log_type, HNDLE hDev);
-INT   EXPRT ybos_magta_write(INT logH, char *pwt, INT nbytes);
-INT   EXPRT ybos_odb_file_dump(INT hBuf, INT ev_ID, INT run_number, char * path);
-INT   EXPRT feodb_file_dump(EQUIPMENT * eqp, char * name, char *pevent, INT run_number, char * path);
-INT   EXPRT file_recompose(char * pevent, INT fmt, char * path, INT file_mode);
-
-void  EXPRT ybk_init(DWORD *pevent);
-void  EXPRT ybk_create(DWORD *pevent, char *bname, DWORD bt, DWORD *pbkdat);
-void  EXPRT ybk_close(DWORD *pevent, void *pbkdat);
-INT   EXPRT ybk_size (DWORD *pevent);
-void  EXPRT ybk_create_chaos(DWORD *pevent, char *bname, DWORD bt, void *pbkdat);
-void  EXPRT ybk_close_chaos(DWORD *pevent, DWORD bt, void *pbkdat);
-
-INT   EXPRT ybk_list(DWORD *pevt , char *bklist);
-INT   EXPRT ybk_find (DWORD *pevt , char * bkname, DWORD * bklength, DWORD * bktype, DWORD **pbkdata);
-INT   EXPRT ybk_iterate (DWORD * pevt , YBOS_BANK_HEADER ** pybkh, void ** pbkdata);
+/* Bank manipulation */
+INT   EXPRT bk_list (BANK_HEADER * pmbh, char * bklist);
+INT   EXPRT bk_find (BANK_HEADER * pmbh, char * bkname, DWORD * bklen, DWORD * bktype, void **pbk);
+void  EXPRT ybk_init        (DWORD *pevent);
+void  EXPRT ybk_create      (DWORD *pevent, char *bname, DWORD btype, void *pbkdat);
+void  EXPRT ybk_close       (DWORD *pevent, void *pbkdat);
+INT   EXPRT ybk_size        (DWORD *pevent);
+INT   EXPRT ybk_list        (DWORD *pevent, char *bklist);
+INT   EXPRT ybk_locate      (DWORD *plrl, char * bkname, void *pdata);
+INT   EXPRT ybk_find        (DWORD *pevent, char *bkname, DWORD *bklength, DWORD *bktype
+                           , void **pbkdata);
+void  EXPRT ybk_create_chaos(DWORD *pevent, char *bname, DWORD btype, void *pbkdat);
+INT   EXPRT ybk_iterate     (DWORD *pevent, YBOS_BANK_HEADER ** pybkh, void ** pdata);
+void  EXPRT ybk_close_chaos (DWORD *pevent, DWORD btype, void *pbkdat);
 
 #ifdef INCLUDE_LOGGING
 INT   EXPRT ybos_log_open(LOG_CHN * log_chn, INT run_number);
 INT   EXPRT ybos_write(LOG_CHN * log_chn, EVENT_HEADER * pevent, INT evt_size);
-INT   EXPRT ybos_flush_buffer(LOG_CHN * log_chn);
 INT   EXPRT ybos_log_close(LOG_CHN * log_chn, INT run_number);
 #endif
-
+/*------------ END --------------------------------------------------------------*/
