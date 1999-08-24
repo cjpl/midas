2dd07b8a16b2c318757b62d9be4fc8bb95e75b15
midas <midas@050218f5-8902-0410-8d0e-8a15d521e4f2>
1999-08-24 14:46:44 +0000

    Added -a flag and indexed variables
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/midas/trunk@381 050218f5-8902-0410-8d0e-8a15d521e4f2

--- utils/odbhist.c
+++ utils/odbhist.c
@@ -6,6 +6,9 @@
   Contents:     MIDAS history display utility
 
   $Log$
+  Revision 1.2  1999/08/24 14:46:44  midas
+  Added -a flag and indexed variables
+
   Revision 1.1  1999/08/23 15:22:39  midas
   Added file
 
@@ -15,16 +18,16 @@
 #include "midas.h"
 #include "msystem.h"
 
-int odb_hist(char *file_name, int run_number, char *var_name, int quiet)
+int odb_hist(char *file_name, int run_number, char *var_name, int quiet, double *value)
 {
 FILE *f;
 char str[256], path[256], key_name[256], line[256];
-int  i;
+int  i, index;
 
   /* assemble file name */
   sprintf(str, file_name, run_number);
 
-  /* split var_name into path and key */
+  /* split var_name into path and key with index */
   strcpy(path, "");
   for (i=strlen(var_name)-1 ; i>=0 && var_name[i] != '/' ; i--);
   if (var_name[i] == '/')
@@ -35,6 +38,13 @@ int  i;
     strcat(path, "]\n");
     strcpy(key_name, var_name+i+1);
     }
+  if (strchr(key_name, '['))
+    {
+    index = atoi(strchr(key_name, '[')+1);
+    *strchr(key_name, '[') = 0;
+    }
+  else
+    index = -1;
 
   f = fopen(str, "r");
   if (f != NULL)
@@ -57,17 +67,39 @@ int  i;
               {
               strcpy(str, line);
               *(strchr(str, '=')-1) = 0;
+              
+              /* check if key name matches */
               if (equal_ustring(str, key_name))
                 {
-                strcpy(str, strchr(line, '=')+2);
-                if (strchr(str, ':') != NULL)
+                if (index == -1)
                   {
-                  strcpy(str, strchr(str, ':')+2);
-                  if (strchr(str, '\n') != NULL)
-                    *strchr(str, '\n') = 0;
-                  if (str[0] == '[' && strchr(str, ']') != NULL)
-                    strcpy(str, strchr(str, ']')+2);
-                  printf(str);
+                  /* non-arrays */
+                  strcpy(str, strchr(line, '=')+2);
+                  if (strchr(str, ':') != NULL)
+                    {
+                    strcpy(str, strchr(str, ':')+2);
+                    if (strchr(str, '\n') != NULL)
+                      *strchr(str, '\n') = 0;
+                    if (str[0] == '[' && strchr(str, ']') != NULL)
+                      strcpy(str, strchr(str, ']')+2);
+                    printf(str);
+                    *value = atof(str);
+                    goto finish;
+                    }
+                  }
+                else
+                  {
+                  /* arrays */
+                  for (i=0 ; i<=index ; i++)
+                    fgets(line, sizeof(line), f);
+                  if (line[0] == '[' && atoi(line+1) == index)
+                    {
+                    strcpy(str, strchr(line, ']')+2);
+                    if (strchr(str, '\n') != NULL)
+                      *strchr(str, '\n') = 0;
+                    printf(str);
+                    *value = atof(str);
+                    }
                   goto finish;
                   }
 
@@ -92,8 +124,9 @@ finish:
 main(int argc, char *argv[])
 {
 DWORD    status, start_run, end_run, run;
-INT      i, quiet;
+INT      i, quiet, add;
 char     file_name[256], var_name[256];
+double   total, value;
 
   /* turn off system message */
   cm_set_msg_print(0, MT_ALL, puts);
@@ -102,6 +135,8 @@ char     file_name[256], var_name[256];
   strcpy(file_name, "run%05d.odb");
   start_run = end_run = 0;
   quiet = FALSE;
+  add = FALSE;
+  total = 0;
 
   /* parse command line parameters */
   for (i=1 ; i<argc ; i++)
@@ -110,6 +145,8 @@ char     file_name[256], var_name[256];
       {
       if (argv[i][1] == 'q')
         quiet = TRUE;
+      else if (argv[i][1] == 'a')
+        add = TRUE;
       else
         {
         if (i+1 >= argc || argv[i+1][0] == '-')
@@ -136,6 +173,7 @@ usage:
       printf("       <varname>        ODB variable name like \"/Runinfo/Run number\"\n");
       printf("       [index]          Index if <varname> is an array\n");
       printf("       -q               Don't display run number\n");
+      printf("       -a               Add numbers for all runs\n");
       return 0;
       }
     }
@@ -149,10 +187,14 @@ usage:
 
   for (run=start_run ; run<=end_run ; run++)
     {
-    status = odb_hist(file_name, run, var_name, quiet);
+    status = odb_hist(file_name, run, var_name, quiet, &value);
     if (status != SUCCESS)
       break;
+    total += value;
     }
+
+  if (add)
+    printf("\nTotal: %lg\n", total);
 
   return 1;
 }
