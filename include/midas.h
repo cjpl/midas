e0c17a4d059f83ba6ddaa5e19105175733498c05
Stefan Ritt <stefan.ritt@psi.ch>
1999-09-14 15:15:45 +0000

    Moved el_xxx funtions into midas.c
    SVN revision: 406

--- include/midas.h
+++ include/midas.h
@@ -8,6 +8,9 @@
 
 
   $Log$
+  Revision 1.36  1999/09/14 15:15:43  midas
+  Moved el_xxx funtions into midas.c
+
   Revision 1.35  1999/09/14 11:46:04  midas
   Added EL_FIRST_MESSAGE
 
@@ -1360,6 +1363,15 @@ INT EXPRT hs_read(DWORD event_id, DWORD start_time, DWORD end_time, DWORD interv
 INT EXPRT hs_dump(DWORD event_id, DWORD start_time, DWORD end_time,
             DWORD interval);
 INT EXPRT hs_fdump(char *file_name, DWORD id);
+
+/*---- ELog functions ----*/
+INT EXPRT el_retrieve(char *tag, char *date, int *run, char *author, char *type, 
+                char *system, char *subject, char *text, int *textsize, 
+                char *orig_tag, char *reply_tag, char *attachment, char *encoding);
+INT EXPRT el_submit(int run, char *author, char *type, char *system, char *subject, 
+              char *text, char *reply_to, char *encoding, char *attachment, char *tag);
+INT EXPRT el_search_message(char *tag, int *fh, BOOL walk);
+INT EXPRT el_search_run(int run, char *return_tag);
 
 /*---- analyzer functions ----*/
 void EXPRT test_register(ANA_TEST *t);
