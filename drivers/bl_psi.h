9b3fd23c344fe5b7c61a8bce39e0fc1202b9c909
Stefan Ritt <stefan.ritt@psi.ch>
1999-11-04 15:54:52 +0000

    Added files
    SVN revision: 567

---
+++ drivers/bl_psi.h
@@ -0,0 +1,16 @@
+/********************************************************************\
+
+  Name:         bl_psi.h
+  Created by:   Stefan Ritt
+
+  Contents:     Urs Rohrer's beamline control device driver function 
+                declarations
+
+  $Log$
+  Revision 1.1  1999/11/04 15:54:52  midas
+  Added files
+
+
+\********************************************************************/
+
+INT bl_psi(INT cmd, ...);
