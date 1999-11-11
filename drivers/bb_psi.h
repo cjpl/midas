0d1c0be3a46c8fbd74d7e2295099747cc118cee2
midas <midas@050218f5-8902-0410-8d0e-8a15d521e4f2>
1999-11-11 10:30:02 +0000

    Added files
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/midas/trunk@589 050218f5-8902-0410-8d0e-8a15d521e4f2

---
+++ drivers/bb_psi.h
@@ -0,0 +1,19 @@
+/********************************************************************\
+
+  Name:         bb_psi.h
+  Created by:   Stefan Ritt
+
+  Contents:     Urs Rohrer's beam blocker control device driver function 
+                declarations
+
+  $Log$
+  Revision 1.1  1999/11/11 10:30:02  midas
+  Added files
+
+  Revision 1.1  1999/11/04 15:54:52  midas
+  Added files
+
+
+\********************************************************************/
+
+INT bb_psi(INT cmd, ...);
