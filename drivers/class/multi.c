32564c6621c717cf4f15f7a68f826dbe64026bba
Stefan Ritt <stefan.ritt@psi.ch>
2000-03-02 21:54:02 +0000

    Added offset in readout routines, added cmd_set_label
    SVN revision: 711

--- drivers/class/multi.c
+++ drivers/class/multi.c
@@ -6,6 +6,9 @@
   Contents:     Multimeter Class Driver
 
   $Log$
+  Revision 1.2  2000/03/02 21:54:02  midas
+  Added offset in readout routines, added cmd_set_label
+
   Revision 1.1  1999/12/20 10:18:16  midas
   Reorganized driver directory structure
 
@@ -441,7 +444,7 @@ MULTI_INFO    *m_info;
 
 /*----------------------------------------------------------------------------*/
 
-INT cd_multi_read(char *pevent)
+INT cd_multi_read(char *pevent, int offset)
 {
 float *pdata;
 DWORD *pdw;
--- drivers/class/multi.h
+++ drivers/class/multi.h
@@ -6,6 +6,9 @@
   Contents:     Multimeter Class Driver Header File
 
   $Log$
+  Revision 1.2  2000/03/02 21:54:02  midas
+  Added offset in readout routines, added cmd_set_label
+
   Revision 1.1  1999/12/20 10:18:16  midas
   Reorganized driver directory structure
 
@@ -17,4 +20,4 @@
 
 /* class driver routines */
 INT cd_multi(INT cmd, PEQUIPMENT pequipment);
-INT cd_multi_read(char *pevent);
+INT cd_multi_read(char *pevent, int);
