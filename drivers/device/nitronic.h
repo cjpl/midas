c1a097e39173cffc79e785d3d4729853606b4f95
midas <midas@050218f5-8902-0410-8d0e-8a15d521e4f2>
2000-10-19 10:56:39 +0000

    Initial revision
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/midas/trunk@932 050218f5-8902-0410-8d0e-8a15d521e4f2

---
+++ drivers/device/nitronic.h
@@ -0,0 +1,16 @@
+/********************************************************************\
+
+  Name:         nitronic.h
+  Created by:   Stefan Ritt
+
+  Contents:     Nitronic HVS 132 device driver function declarations
+
+  $Log$
+  Revision 1.1  2000/10/19 10:56:39  midas
+  Initial revision
+
+
+\********************************************************************/
+
+INT nitronic(INT cmd, ...);
+
