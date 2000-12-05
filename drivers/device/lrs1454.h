cd3fa69ee3454e7184db227d1f11903364b39f63
Stefan Ritt <stefan.ritt@psi.ch>
2000-12-05 00:49:55 +0000

    Initial revision
    SVN revision: 975

---
+++ drivers/device/lrs1454.h
@@ -0,0 +1,16 @@
+/********************************************************************\
+
+  Name:         lrs1454.h
+  Created by:   Stefan Ritt
+
+  Contents:     lrs1454 device driver function declarations
+
+  $Log$
+  Revision 1.1  2000/12/05 00:49:55  midas
+  Initial revision
+
+
+\********************************************************************/
+
+INT lrs1454(INT cmd, ...);
+
