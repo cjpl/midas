ff787646284b79a8c8f7fd809b0eebb93487c5f1
midas <midas@050218f5-8902-0410-8d0e-8a15d521e4f2>
2001-01-03 16:07:24 +0000

    CR-LF converted
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/midas/trunk@982 050218f5-8902-0410-8d0e-8a15d521e4f2

--- drivers/device/lrs1454.h
+++ drivers/device/lrs1454.h
@@ -6,9 +6,11 @@
   Contents:     lrs1454 device driver function declarations
 
   $Log$
+  Revision 1.2  2001/01/03 16:07:24  midas
+  CR-LF converted
+
   Revision 1.1  2000/12/05 00:49:55  midas
   Initial revision
-
 
 \********************************************************************/
 
