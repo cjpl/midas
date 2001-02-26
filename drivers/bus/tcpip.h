3286ad4397bcf9620a49360953d65b329d6dd5e8
midas <midas@050218f5-8902-0410-8d0e-8a15d521e4f2>
2001-02-26 13:58:06 +0000

    Added files
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/midas/trunk@1014 050218f5-8902-0410-8d0e-8a15d521e4f2

---
+++ drivers/bus/tcpip.h
@@ -0,0 +1,16 @@
+/********************************************************************\
+
+  Name:         tcpip.h
+  Created by:   Stefan Ritt
+
+  Contents:     Header file for TCPIP bus driver
+
+  $Log$
+  Revision 1.1  2001/02/26 13:58:06  midas
+  Added files
+
+\********************************************************************/
+
+INT tcpip(INT cmd, ...);
+
+
