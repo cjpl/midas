d38fa0e3e8bb6c0b4982d4d1e27647a129979a75
Stefan Ritt <stefan.ritt@psi.ch>
2001-08-14 10:04:56 +0000

    Use iopl() instead of ioperm() to access PCI ports > 0x3FF
    SVN revision: 1142

--- utils/dio.c
+++ utils/dio.c
@@ -11,6 +11,9 @@
                 dio frontend
 
   $Log$
+  Revision 1.2  2001/08/14 10:04:56  midas
+  Use iopl() instead of ioperm() to access PCI ports > 0x3FF
+
   Revision 1.1  1999/12/20 09:19:11  midas
   Moved dio.c from drivers to utils direcotry
 
@@ -27,31 +30,20 @@
 #include <string.h>
 #include <unistd.h>
 
-/* open all ports between 0x200 and ox3FF */
-static const int iobase = 0x200;
-static const int num_ports = 0x200;
-
 int main( int argc, char **argv )
 {
   int i;
   int status;
 
-  /* Grant access to the 0x80 ioports 
-     This is for being able to use the i/o port function with
-     _P (pause) for slow access.
-     See macros in hyt1331.c
-     See asm/io.h  port 0x80 is not supposed to be used */
-  status = ioperm( 0x80, 1, 1 );
-
   /* Grant access to the device's ioports */
-  status = ioperm( iobase, num_ports, 1 );
+  status = iopl( 3 );
   if (status < 0) {
-    perror( "ioperm()" );
+    perror( "iopl()" );
     exit( 2 );
   }
 
   /* Surrender root privileges - the exec()ed program will keep IO
-     access to the IO ports (see "man 2 ioperm") */
+     access to the IO ports (see "man 2 iopl") */
   if (setuid( getuid() ) < 0) {
     perror( "setuid()" );
     exit( 2 );
