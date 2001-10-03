4a4478424442ca9f0d0aa66b6baebe81204efb34
Stefan Ritt <stefan.ritt@psi.ch>
2001-10-03 14:45:11 +0000

    Separated IOCTL functions code into header file
    SVN revision: 1177

--- drivers/kernel/khyt1331/khyt1331.c
+++ drivers/kernel/khyt1331/khyt1331.c
@@ -6,6 +6,9 @@
   Contents:     Kernel mode driver for Hytec 1331 CAMAC interface
 
   $Log$
+  Revision 1.2  2001/10/03 14:45:11  midas
+  Separated IOCTL functions code into header file
+
   Revision 1.1  2001/08/21 14:28:34  midas
   Initial revision
 
@@ -46,45 +49,7 @@ EXPORT_NO_SYMBOLS;
 /* LOCAL/EXPERIMENTAL use, see devices.txt */
 #define HYT1331_MAJOR         60
 
-/* IOCTL operation codes */
-#define OP_CRATE_ZINIT         1
-#define OP_CRATE_CLEAR         2
-#define OP_CRATE_CHECK         3
-
-#define OP_CNAF8              10
-#define OP_CNAF16             11
-#define OP_CNAF24             12
-
-#define OP_CNAF8R             15
-#define OP_CNAF16R            16
-#define OP_CNAF24R            17
-
-#define OP_INHIBIT_SET        20
-#define OP_INHIBIT_CLEAR      21
-#define OP_INHIBIT_TEST       22
-#define OP_LAM_ENABLE         23
-#define OP_LAM_DISABLE        24
-#define OP_LAM_READ           25
-#define OP_LAM_CLEAR          26
-#define OP_INTERRUPT_ENABLE   27
-#define OP_INTERRUPT_DISABLE  28
-#define OP_INTERRUPT_TEST     29
-
-/* OP_CNAFxR repeat modes */
-#define RM_COUNT               1
-#define RM_QMODE               2
-#define RM_ASCAN               3
-#define RM_NSCAN               4
-
-#ifndef EINAVL
-#define EINVAL                22
-#endif
-
-typedef struct
-  {
-  int c, n, a, f, x, q, r, m;
-  unsigned long d;
-  } CNAF_BUF;
+#include "khyt1331.h"
 
 #define OUTP(_p, _d) outb(_d, _p)
 #define OUTPW(_p, _d) outw(_d, _p)
