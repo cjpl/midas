edcbe6115f7b722978c3e179351ae678d0338599
Stefan Ritt <stefan.ritt@psi.ch>
2002-05-29 13:24:56 +0000

    Added stdlib.h and stdarg.h
    SVN revision: 1490

--- drivers/bus/null.c
+++ drivers/bus/null.c
@@ -6,6 +6,9 @@
   Contents:     NULL bus driver
 
   $Log$
+  Revision 1.2  2002/05/29 13:24:56  midas
+  Added stdlib.h and stdarg.h
+
   Revision 1.1  2002/03/14 13:12:17  midas
   Split null driver in bus and device drivers
 
@@ -13,6 +16,8 @@
 \********************************************************************/
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
 #include "midas.h"
 
 static int debug_flag = 0;
