909356d6ea63adeddc191898a081e312a1811877
midas <midas@050218f5-8902-0410-8d0e-8a15d521e4f2>
2002-07-05 15:27:39 +0000

    Initial revision
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/midas/trunk/mscb@1527 050218f5-8902-0410-8d0e-8a15d521e4f2

--- embedded/mscbutil.c
+++ embedded/mscbutil.c
@@ -6,6 +6,9 @@
   Contents:     Various utility functions for MSCB protocol
 
   $Log$
+  Revision 1.2  2002/07/05 15:27:39  midas
+  Initial revision
+
   Revision 1.1  2002/07/05 08:12:46  midas
   Moved files
 
@@ -14,118 +17,113 @@
 #include "mscb.h"
 #include <intrins.h>
 
-/*
-;--------------------------------------------------------------------
-;
-; CRC-8 code calculation, adds value in A to running CRC code 
-; in CRC_CODE
-;
-; Modifies: DPTR,CRC_CODE
-;
-;--------------------------------------------------------------------
+/*------------------------------------------------------------------*/
 
-CRC8_DATA:       
-        db 000h, 05eh, 0bch, 0e2h, 061h, 03fh, 0ddh, 083h
-        db 0c2h, 09ch, 07eh, 020h, 0a3h, 0fdh, 01fh, 041h
-        db 09dh, 0c3h, 021h, 07fh, 0fch, 0a2h, 040h, 01eh
-        db 05fh, 001h, 0e3h, 0bdh, 03eh, 060h, 082h, 0dch
-        db 023h, 07dh, 09fh, 0c1h, 042h, 01ch, 0feh, 0a0h
-        db 0e1h, 0bfh, 05dh, 003h, 080h, 0deh, 03ch, 062h
-        db 0beh, 0e0h, 002h, 05ch, 0dfh, 081h, 063h, 03dh
-        db 07ch, 022h, 0c0h, 09eh, 01dh, 043h, 0a1h, 0ffh
-        db 046h, 018h, 0fah, 0a4h, 027h, 079h, 09bh, 0c5h
-        db 084h, 0dah, 038h, 066h, 0e5h, 0bbh, 059h, 007h
-        db 0dbh, 085h, 067h, 039h, 0bah, 0e4h, 006h, 058h
-        db 019h, 047h, 0a5h, 0fbh, 078h, 026h, 0c4h, 09ah
-        db 065h, 03bh, 0d9h, 087h, 004h, 05ah, 0b8h, 0e6h
-        db 0a7h, 0f9h, 01bh, 045h, 0c6h, 098h, 07ah, 024h
-        db 0f8h, 0a6h, 044h, 01ah, 099h, 0c7h, 025h, 07bh
-        db 03ah, 064h, 086h, 0d8h, 05bh, 005h, 0e7h, 0b9h
-        db 08ch, 0d2h, 030h, 06eh, 0edh, 0b3h, 051h, 00fh
-        db 04eh, 010h, 0f2h, 0ach, 02fh, 071h, 093h, 0cdh
-        db 011h, 04fh, 0adh, 0f3h, 070h, 02eh, 0cch, 092h
-        db 0d3h, 08dh, 06fh, 031h, 0b2h, 0ech, 00eh, 050h
-        db 0afh, 0f1h, 013h, 04dh, 0ceh, 090h, 072h, 02ch
-        db 06dh, 033h, 0d1h, 08fh, 00ch, 052h, 0b0h, 0eeh
-        db 032h, 06ch, 08eh, 0d0h, 053h, 00dh, 0efh, 0b1h
-        db 0f0h, 0aeh, 04ch, 012h, 091h, 0cfh, 02dh, 073h
-        db 0cah, 094h, 076h, 028h, 0abh, 0f5h, 017h, 049h
-        db 008h, 056h, 0b4h, 0eah, 069h, 037h, 0d5h, 08bh
-        db 057h, 009h, 0ebh, 0b5h, 036h, 068h, 08ah, 0d4h
-        db 095h, 0cbh, 029h, 077h, 0f4h, 0aah, 048h, 016h
-        db 0e9h, 0b7h, 055h, 00bh, 088h, 0d6h, 034h, 06ah
-        db 02bh, 075h, 097h, 0c9h, 04ah, 014h, 0f6h, 0a8h
-        db 074h, 02ah, 0c8h, 096h, 015h, 04bh, 0a9h, 0f7h
-        db 0b6h, 0e8h, 00ah, 054h, 0d7h, 089h, 06bh, 035h
+unsigned char code crc8_data[] = {
+0x00, 0x5e, 0xbc, 0xe2, 0x61, 0x3f, 0xdd, 0x83,
+0xc2, 0x9c, 0x7e, 0x20, 0xa3, 0xfd, 0x1f, 0x41,
+0x9d, 0xc3, 0x21, 0x7f, 0xfc, 0xa2, 0x40, 0x1e,
+0x5f, 0x01, 0xe3, 0xbd, 0x3e, 0x60, 0x82, 0xdc,
+0x23, 0x7d, 0x9f, 0xc1, 0x42, 0x1c, 0xfe, 0xa0,
+0xe1, 0xbf, 0x5d, 0x03, 0x80, 0xde, 0x3c, 0x62,
+0xbe, 0xe0, 0x02, 0x5c, 0xdf, 0x81, 0x63, 0x3d,
+0x7c, 0x22, 0xc0, 0x9e, 0x1d, 0x43, 0xa1, 0xff,
+0x46, 0x18, 0xfa, 0xa4, 0x27, 0x79, 0x9b, 0xc5,
+0x84, 0xda, 0x38, 0x66, 0xe5, 0xbb, 0x59, 0x07,
+0xdb, 0x85, 0x67, 0x39, 0xba, 0xe4, 0x06, 0x58,
+0x19, 0x47, 0xa5, 0xfb, 0x78, 0x26, 0xc4, 0x9a,
+0x65, 0x3b, 0xd9, 0x87, 0x04, 0x5a, 0xb8, 0xe6,
+0xa7, 0xf9, 0x1b, 0x45, 0xc6, 0x98, 0x7a, 0x24,
+0xf8, 0xa6, 0x44, 0x1a, 0x99, 0xc7, 0x25, 0x7b,
+0x3a, 0x64, 0x86, 0xd8, 0x5b, 0x05, 0xe7, 0xb9,
+0x8c, 0xd2, 0x30, 0x6e, 0xed, 0xb3, 0x51, 0x0f,
+0x4e, 0x10, 0xf2, 0xac, 0x2f, 0x71, 0x93, 0xcd,
+0x11, 0x4f, 0xad, 0xf3, 0x70, 0x2e, 0xcc, 0x92,
+0xd3, 0x8d, 0x6f, 0x31, 0xb2, 0xec, 0x0e, 0x50,
+0xaf, 0xf1, 0x13, 0x4d, 0xce, 0x90, 0x72, 0x2c,
+0x6d, 0x33, 0xd1, 0x8f, 0x0c, 0x52, 0xb0, 0xee,
+0x32, 0x6c, 0x8e, 0xd0, 0x53, 0x0d, 0xef, 0xb1,
+0xf0, 0xae, 0x4c, 0x12, 0x91, 0xcf, 0x2d, 0x73,
+0xca, 0x94, 0x76, 0x28, 0xab, 0xf5, 0x17, 0x49,
+0x08, 0x56, 0xb4, 0xea, 0x69, 0x37, 0xd5, 0x8b,
+0x57, 0x09, 0xeb, 0xb5, 0x36, 0x68, 0x8a, 0xd4,
+0x95, 0xcb, 0x29, 0x77, 0xf4, 0xaa, 0x48, 0x16,
+0xe9, 0xb7, 0x55, 0x0b, 0x88, 0xd6, 0x34, 0x6a,
+0x2b, 0x75, 0x97, 0xc9, 0x4a, 0x14, 0xf6, 0xa8,
+0x74, 0x2a, 0xc8, 0x96, 0x15, 0x4b, 0xa9, 0xf7,
+0xb6, 0xe8, 0x0a, 0x54, 0xd7, 0x89, 0x6b, 0x35,
+};
 
-CRC8_ADD:
-        PUSH    ACC
+unsigned char crc8(unsigned char *buffer, int len)
+/********************************************************************\
 
-        MOV     DPTR,#CRC8_DATA         ; Point To Table
-        XRL     A,CRC_CODE              ; XOR In CRC
-        MOVC    A,@A+DPTR               ; Get New CRC Byte
-        MOV     CRC_CODE,A              ; Store Back
+  Routine: crc8
 
-        POP     ACC
-        RET
+  Purpose: Calculate 8-bit cyclic redundancy checksum
 
-CRC8_ADD1:
-        PUSH    ACC
-        PUSH    DPL
-        PUSH    DPH
+  Input:
+    unsigned char *data     data buffer
+    int len                 data length in bytes
 
-        MOV     DPTR,#CRC8_DATA         ; Point To Table
-        XRL     A,CRC_CODE              ; XOR In CRC
-        MOVC    A,@A+DPTR               ; Get New CRC Byte
-        MOV     CRC_CODE,A              ; Store Back
 
-        POP     DPH
-        POP     DPL
-        POP     ACC
-        RET
+  Function value:
+    unsighend char          CRC-8 code
 
-;--------------------------------------------------------------------
-;
-; Configure UART via timer 2 
-;
-; parameters:
-;   A=1:9600,2:19200,3:28800,4:57600,5:115200,6:172800,7:345600 Baud
-;
-; Modifes: A,DPTR
-;
-;--------------------------------------------------------------------
+\********************************************************************/
+{
+int i;
+unsigned char crc8_code, index;
 
-BAUD_TABLE:     ; reload 100-x, x = 11.0592MHz/32/baud
-        DB      100h-36,100h-18,100h-12,100h-6,100h-3,100h-2,100h-1
+  crc8_code = 0;
+  for (i=0 ; i<len ; i++)
+    {
+    index = buffer[i] ^ crc8_code;
+    crc8_code = crc8_data[index];
+    }
 
-UART_INIT:
-        MOV     SCON,#11010000b ; Mode 3, 9 bit, receive enable
-;        MOV     SCON,#50H       ; Mode 1, 8 bit, receive enable
+  return crc8_code;
+}
 
-        MOV     TMOD,#00H       ; timer 0&1 no function
-        MOV     T2CON,#00110100b; timer 2 RX+TX mode
-        MOV     RCAP2H,#0FFh
-        MOV     RCAP2L,#100h-36 ; 9600 baud by default
-        SETB    TR2
+/*------------------------------------------------------------------*/
 
-        RET
+void uart_init(unsigned char baud)
+/********************************************************************\
 
-SET_BAUD:
-        CLR     TR2
-        MOV     RCAP2H,#0FFh
+  Routine: uart_init
 
-        MOV     DPTR,#BAUD_TABLE
-        DEC     A
-        MOVC    A,@A+DPTR
-        MOV     RCAP2L,A        ; set reload register from table
-        SETB    TR2
+  Purpose: Initial serial interface
 
-        RET
-*/
+  Input:
+    unsigned char baud      1:9600,2:19200,3:28800,4:57600,
+                            5:115200,6:172800,7:345600 Baud
 
-/*---- Refresh watchdog --------------------------------------------*/
+
+\********************************************************************/
+{
+unsigned char baud_table[] = 
+  {0x100-36, 0x100-18, 0x100-12, 0x100-6, 0x100-3, 0x100-2, 0x100-1};
+
+
+  SCON = 0xD0;   // Mode 3, 9 bit, receive enable
+// SCON = 0x50;    // Mode 1, 8 bit, receive enable
+
+  TMOD = 0x00;   // timer 0&1 no function
+  T2CON = 0x34;  // timer 2 RX+TX mode
+  RCAP2H = 0xFF;
+  RCAP2L = baud_table[baud-1];
+}
+
+/*------------------------------------------------------------------*/
 
 void watchdog_refresh(void)
+/********************************************************************\
+
+  Routine: watchdog_refresh
+
+  Purpose: Refresh watchdog, has to be called regularly, otherwise
+           the watchdog issues a reset
+
+\********************************************************************/
 {
 #ifdef CPU_ADUC812
   WDR1 = 1;
@@ -139,9 +137,13 @@ void watchdog_refresh(void)
 
 /*------------------------------------------------------------------*\
 
-  Delay functions for 11.0520 MHz quartz
+/********************************************************************\
 
-\*------------------------------------------------------------------*/
+  Routine: delay_ms, delay_us
+
+  Purpose: Delay functions for 11.0520 MHz quartz
+
+\********************************************************************/
 
 #ifdef CPU_ADUC812
 #endif
@@ -183,36 +185,24 @@ unsigned int  remaining_us;
 }
 #endif
 
-/*
-;--------------------------------------------------------------------
-;
-; EEPROM functions for reading writing flash EEPROM
-;
-; Parameters: 
-;   A   parameter index 0..N
-;   R0  pointer to data
-;   R1  data length (1..4)
-;
-; Modifies:
-;   A,R0,R1,R2
-;
-;--------------------------------------------------------------------
+/*------------------------------------------------------------------*/
 
-IF(CPU_ADUC812)
+void eeprom_read(unsigned char idata *dst, unsigned char len, 
+                 unsigned char offset)
+/********************************************************************\
 
-EEPROM_WRITE:
-        MOV     EADRL,A         ; select page
-        MOV     ECON,#5         ; erase page
-        MOV     EDATA1,@R0
-        INC     R0
-        MOV     EDATA2,@R0
-        INC     R0
-        MOV     EDATA3,@R0
-        INC     R0
-        MOV     EDATA4,@R0
-        INC     R0
-        MOV     ECON,#2         ; write page
-        RET
+  Routine: eeprom_read
+
+  Purpose: Read from internal EEPROM
+
+  Input:
+    unsigned char idata *dst    Destination in IDATA memory
+    unsigned char len           Number of bytes to copy
+    unsigend char offset        Offset in EEPROM in bytes
+
+\********************************************************************/
+{
+#ifdef CPU_ADUC812
 
 EEPROM_READ:
         MOV     EADRL,A         ; set page
@@ -235,93 +225,90 @@ EEPROM_READ:
 EEPROM_RET:
         RET
 
-ENDIF
+#endif
 
-IF(CPU_C8051F000)
+#ifdef CPU_C8051F000
+
+  unsigned char i;
+  unsigned char code *p;
+
+  p = 0x8000; // read from 128-byte EEPROM page
+  p += offset;
+
+  for (i=0 ; i<len ; i++)
+    dst[i] = p[i];
+
+#endif
+}
+
+/*------------------------------------------------------------------*/
+
+void eeprom_write(unsigned char idata *src, unsigned char len, 
+                  unsigned char offset)
+/********************************************************************\
+
+  Routine: eeprom_read
+
+  Purpose: Read from internal EEPROM
+
+  Input:
+    unsigned char idata *src    Source in IDATA memory
+    unsigned char len           Number of bytes to copy
+    unsigend char offset        Offset in EEPROM in bytes
+
+\********************************************************************/
+{
+#ifdef CPU_ADUC812
 
 EEPROM_WRITE:
-        ; copy page to RAM at 128
-        PUSH    DPL
-        PUSH    DPH
-        PUSH    B
-        PUSH    ACC
-        MOV     B,R0
-        MOV     R0,#80h
-        MOV     DPTR,#7F80h
-        MOV     R1,#80h
-EEW_LOOP1:
-        MOV     A,R1
-        MOVC    A,@A+DPTR
-        MOV     @R1,A
-        INC     R1
-        DJNZ    R0,EEW_LOOP1
-
-        ; copy data into RAM page
-        POP     ACC
-        MOV     R0,B
-
-        RL      A
-        RL      A
-        ADD     A,#80h
-        MOV     R1,A
-        MOV     R2,#4
-EEW_LOOP2:
-        MOV     A,@R0
-        MOV     @R1,A
-        INC     R1
+        MOV     EADRL,A         ; select page
+        MOV     ECON,#5         ; erase page
+        MOV     EDATA1,@R0
         INC     R0
-        DJNZ    R2,EEW_LOOP2
-
-        ; erase page
-        MOV     A,FLSCL
-        ANL     A,#0F0h
-        ORL     A,#00001000b    ; set timer for 11.052 MHz clock
-        MOV     FLSCL,A
-        MOV     PSCTL,#3        ; allow write+erase
-        MOV     DPTR,#8000h
-        MOVX    @DPTR,A
-
-        ; write page
-        MOV     PSCTL,#1        ; allow write
-
-        MOV     R0,#80h
-        MOV     DPTR,#8000h
-        MOV     R1,#80h
-EEW_LOOP3:
-        MOV     A,@R1
-        MOVX    @DPTR,A
-        INC     R1
-        INC     DPL
-        DJNZ    R0,EEW_LOOP3
-
-        MOV     PSCTL,#0        ; don't allow write+erase
-
-        POP     B
-        POP     DPH
-        POP     DPL
+        MOV     EDATA2,@R0
+        INC     R0
+        MOV     EDATA3,@R0
+        INC     R0
+        MOV     EDATA4,@R0
+        INC     R0
+        MOV     ECON,#2         ; write page
         RET
 
-EEPROM_READ:
-        MOV     DPTR,#8000h     ; read from 128-byte EEPROM page
-        RL      A
-        RL      A
-EER_LOOP:
-        PUSH    ACC
-        MOVC    A,@A+DPTR
-        MOV     @R0,A
-        POP     ACC
-        INC     R0
-        INC     A
-        DJNZ    R1,EER_LOOP
-        RET
 
-ENDIF
-*/
+#endif
+
+#ifdef CPU_C8051F000
+  unsigned char xdata *p;
+  unsigned char i;
+
+  FLSCL = (FLSCL & 0xF0) | 0x08; // set timer for 11.052 MHz clock
+  PSCTL = 0x03;                  // allow write and erase
+
+  p = 0x8000;                    // erase page
+  *p = 0;
+
+  PSCTL = 0x01;                  // allow write
+
+  p = 0x8000 + offset;           // write data
+  for (i=0 ; i<len ; i++)
+    p[i] = src[i];
+
+  PSCTL = 0x00;                  // don't allow write
+#endif
+}
+
 /*------------------------------------------------------------------*\
 
-  LCD functions for HD44780/KS0066 compatible LCD displays
+/********************************************************************\
 
-\*------------------------------------------------------------------*/
+  Routine: lcd_setup, lcd_clear, lcd_goto, lcd_puts, putchar
+
+  Purpose: LCD functions for HD44780/KS0066 compatible LCD displays
+
+           Since putchar is used by printf, this function puts its
+           output to the LCD as well
+
+\********************************************************************/
 
 #define LCD P2 // LCD display connected to port2
 
@@ -339,6 +326,8 @@ sbit LCD_P_W    = LCD ^ 6;
 sbit LCD_SD     = LCD ^ 0;
 
 bit lcd_present;
+
+/*------------------------------------------------------------------*/
 
 lcd_out(unsigned char d, bit df)
 {
@@ -384,6 +373,8 @@ lcd_out(unsigned char d, bit df)
   delay_us(1);
 }
 
+/*------------------------------------------------------------------*/
+
 void lcd_setup()
 {
   LCD = 0;
@@ -426,10 +417,14 @@ void lcd_setup()
   lcd_out(0x06, 0); // entry mode
 }
 
+/*------------------------------------------------------------------*/
+
 void lcd_clear()
 {
   lcd_out(0x01, 0);
 }
+
+/*------------------------------------------------------------------*/
 
 void lcd_goto(char x, char y)
 {
@@ -438,11 +433,15 @@ void lcd_goto(char x, char y)
   lcd_out((x & 0x0F) | (0x80) | ((y & 0x01) << 6), 0);
 }
 
+/*------------------------------------------------------------------*/
+
 char putchar(char c)
 {
   lcd_out(c, 1);
   return c;
 }
+
+/*------------------------------------------------------------------*/
 
 void lcd_puts(char *str)
 {
@@ -450,14 +449,13 @@ void lcd_puts(char *str)
     lcd_out(*str++, 1);
 }
 
+/********************************************************************\
 
+  Routine: scs_lcd1_read
 
-/*------------------------------------------------------------------*\
+  Purpose: Returns switch (button) state of SCS-LCD1 module
 
-  Input function for SCS-LCD1
-  Returns: switch states in ACC
-
-\*------------------------------------------------------------------*/
+\********************************************************************/
 
 char scs_lcd1_read()
 {
