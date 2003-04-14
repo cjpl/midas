f792451c509472daf84cff5870565b30be9ada32
midas <midas@050218f5-8902-0410-8d0e-8a15d521e4f2>
2003-04-14 13:28:33 +0000

    Made ADC0 bank a structured bank
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/midas/trunk@1742 050218f5-8902-0410-8d0e-8a15d521e4f2

--- examples/experiment/experim.h
+++ examples/experiment/experim.h
@@ -17,27 +17,91 @@
                 tation which can be used in the db_create_record function
                 to setup an ODB structure which matches the C structure.
 
-  Created on:   Fri Apr 11 12:58:35 2003
+  Created on:   Mon Apr 14 15:27:19 2003
 
 \********************************************************************/
 
-#ifndef EXCL_TRIGGER
-
-#define TEST_BANK_DEFINED
+#define EXP_PARAM_DEFINED
 
 typedef struct {
-  float     a;
-  float     b;
-  char      str[32];
-} TEST_BANK;
+  char      comment[80];
+} EXP_PARAM;
 
-#define TEST_BANK_STR(_name) char *_name[] = {\
+#define EXP_PARAM_STR(_name) char *_name[] = {\
 "[.]",\
-"a = FLOAT : 1.234",\
-"b = FLOAT : 2.24923e+006",\
-"str = STRING : [32] Hello",\
+"Comment = STRING : [80] Test",\
 "",\
 NULL }
+
+#ifndef EXCL_ADC_CALIBRATION
+
+#define ADC_CALIBRATION_PARAM_DEFINED
+
+typedef struct {
+  INT       pedestal[8];
+  float     software_gain[8];
+  double    histo_threshold;
+} ADC_CALIBRATION_PARAM;
+
+#define ADC_CALIBRATION_PARAM_STR(_name) char *_name[] = {\
+"[.]",\
+"Pedestal = INT[8] :",\
+"[0] 174",\
+"[1] 194",\
+"[2] 176",\
+"[3] 182",\
+"[4] 185",\
+"[5] 215",\
+"[6] 202",\
+"[7] 202",\
+"Software Gain = FLOAT[8] :",\
+"[0] 1",\
+"[1] 1",\
+"[2] 1",\
+"[3] 1",\
+"[4] 1",\
+"[5] 1",\
+"[6] 1",\
+"[7] 1",\
+"Histo threshold = DOUBLE : 20",\
+"",\
+NULL }
+
+#endif
+
+#ifndef EXCL_ADC_SUMMING
+
+#define ADC_SUMMING_PARAM_DEFINED
+
+typedef struct {
+  float     adc_threshold;
+} ADC_SUMMING_PARAM;
+
+#define ADC_SUMMING_PARAM_STR(_name) char *_name[] = {\
+"[.]",\
+"ADC threshold = FLOAT : 5",\
+"",\
+NULL }
+
+#endif
+
+#ifndef EXCL_GLOBAL
+
+#define GLOBAL_PARAM_DEFINED
+
+typedef struct {
+  float     adc_threshold;
+} GLOBAL_PARAM;
+
+#define GLOBAL_PARAM_STR(_name) char *_name[] = {\
+"[.]",\
+"ADC Threshold = FLOAT : 5",\
+"",\
+NULL }
+
+#endif
+
+#ifndef EXCL_TRIGGER
 
 #define ADC0_BANK_DEFINED
 
@@ -54,6 +118,18 @@ typedef struct {
 "adc1 = WORD : 0",\
 "adc2 = WORD : 0",\
 "adc3 = WORD : 0",\
+"",\
+NULL }
+
+#define ASUM_BANK_DEFINED
+
+typedef struct {
+  float     sum;
+} ASUM_BANK;
+
+#define ASUM_BANK_STR(_name) char *_name[] = {\
+"[.]",\
+"Sum = FLOAT : 0",\
 "",\
 NULL }
 
@@ -93,7 +169,19 @@ typedef struct {
 "Log history = INT : 0",\
 "Frontend host = STRING : [32] pc810",\
 "Frontend name = STRING : [32] Sample Frontend",\
-"Frontend file name = STRING : [256] c:\online\frontend.c",\
+"Frontend file name = STRING : [256] C:\Midas\examples\experiment\frontend.c",\
+"",\
+NULL }
+
+#define TRIGGER_SETTINGS_DEFINED
+
+typedef struct {
+  BYTE      io506;
+} TRIGGER_SETTINGS;
+
+#define TRIGGER_SETTINGS_STR(_name) char *_name[] = {\
+"[.]",\
+"IO506 = BYTE : 7",\
 "",\
 NULL }
 
@@ -137,7 +225,7 @@ typedef struct {
 "Log history = INT : 0",\
 "Frontend host = STRING : [32] pc810",\
 "Frontend name = STRING : [32] Sample Frontend",\
-"Frontend file name = STRING : [256] c:\online\frontend.c",\
+"Frontend file name = STRING : [256] C:\Midas\examples\experiment\frontend.c",\
 "",\
 NULL }
 
