d379b6e7839b5cbba61ed049e796a6852e7404df
midas <midas@050218f5-8902-0410-8d0e-8a15d521e4f2>
2003-04-25 13:20:08 +0000

    Initial revision
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/midas/trunk@1803 050218f5-8902-0410-8d0e-8a15d521e4f2

---
+++ examples/hbookexpt/analyzer.h
@@ -0,0 +1,31 @@
+/********************************************************************\
+
+  Name:         analyzer.h
+  Created by:   Stefan Ritt
+
+  Contents:     Analyzer global include file
+
+  $Log$
+  Revision 1.1  2003/04/25 13:20:07  midas
+  Initial revision
+
+  Revision 1.3  2003/04/23 15:08:57  midas
+  Decreased N_TDC to 4
+
+  Revision 1.2  1998/10/12 12:18:58  midas
+  Added Log tag in header
+
+
+\********************************************************************/
+                                                        
+/*-- Parameters ----------------------------------------------------*/
+
+/* number of channels */
+#define N_ADC               4
+#define N_TDC               4
+#define N_SCLR              4
+
+/*-- Histo ID bases ------------------------------------------------*/
+
+#define ADCCALIB_ID_BASE 1000
+#define ADCSUM_ID_BASE   2000
