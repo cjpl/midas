cbbb18986ab5e6eb6193165f5634b3367f1f9f4c
midas <midas@050218f5-8902-0410-8d0e-8a15d521e4f2>
2003-05-12 10:30:16 +0000

    Initial revision
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/midas/trunk@1847 050218f5-8902-0410-8d0e-8a15d521e4f2

---
+++ drivers/device/mscbdev.h
@@ -0,0 +1,15 @@
+/********************************************************************\
+
+  Name:         mscbdev.h
+  Created by:   Stefan Ritt
+
+  Contents:     Device driver function declarations for MSCB device
+
+  $Log$
+  Revision 1.1  2003/05/12 10:30:16  midas
+  Initial revision
+
+
+\********************************************************************/
+
+INT mscbdev(INT cmd, ...);
