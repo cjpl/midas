96d8476248aa05e171011322592d6f85083835c6
Stefan Ritt <stefan.ritt@psi.ch>
2003-06-05 07:26:46 +0000

    Initial revision
    SVN revision: 1877

---
+++ mscb/embedded/scs_520.c
@@ -0,0 +1,328 @@
+/********************************************************************\
+
+  Name:         scs_520.c
+  Created by:   Stefan Ritt
+
+
+  Contents:     Application specific (user) part of
+                Midas Slow Control Bus protocol 
+                for SCS-520 analog I/O with current option
+
+  $Log$
+  Revision 1.1  2003/06/05 07:26:46  midas
+  Initial revision
+
+\********************************************************************/
+
+#include <stdio.h>
+#include "mscb.h"
+
+extern bit FREEZE_MODE;
+extern bit DEBUG_MODE;
+
+char code node_name[] = "SCS-520";
+
+sbit SR_CLOCK   = P0 ^ 4;    // Shift register clock
+sbit SR_STROBE  = P0 ^ 5;    // Storage register clock
+sbit SR_DATA    = P0 ^ 6;    // Serial data
+
+/*---- Define variable parameters returned to CMD_GET_INFO command ----*/
+
+/* data buffer (mirrored in EEPROM) */
+
+struct {
+  float         adc[8];
+  float         dac0, dac1;
+  unsigned char p1;
+  unsigned char adc_average;
+  unsigned char gain[8];  // PGA bits
+  float         gain_cal; // gain calibration
+  float         bip_cal;  // bipolar zero offset
+} idata user_data;
+  
+float idata gain[8];     // gain resulting from PGA bits
+
+/* Usage of gain:
+
+ Bipol.     Ext.  PGA          Int. PGA       Gain
+
+  Bit5      Bit4 Bit3       Bit2 Bit1 Bit0
+   0         0    0          0    0    0       1
+   0         0    0          0    0    1       2
+   0         0    0          0    1    0       4
+   0         0    0          0    1    1       8
+   0         0    0          1    0    0      16
+   0         0    0          1    1    0       0.5
+
+   0         0    1          0    0    0      10
+   0         1    0          0    0    0     100
+   0         1    1          0    0    0    1000
+
+   1  bipolar, represents external jumper setting
+*/
+
+
+MSCB_INFO_VAR code variables[] = {
+  4, UNIT_VOLT,   0, 0, MSCBF_FLOAT, "ADC0",    &user_data.adc[0],
+  4, UNIT_VOLT,   0, 0, MSCBF_FLOAT, "ADC1",    &user_data.adc[1],
+  4, UNIT_VOLT,   0, 0, MSCBF_FLOAT, "ADC2",    &user_data.adc[2],
+  4, UNIT_VOLT,   0, 0, MSCBF_FLOAT, "ADC3",    &user_data.adc[3],
+  4, UNIT_VOLT,   0, 0, MSCBF_FLOAT, "ADC4",    &user_data.adc[4],
+  4, UNIT_VOLT,   0, 0, MSCBF_FLOAT, "ADC5",    &user_data.adc[5],
+  4, UNIT_VOLT,   0, 0, MSCBF_FLOAT, "ADC6",    &user_data.adc[6],
+  4, UNIT_VOLT,   0, 0, MSCBF_FLOAT, "ADC7",    &user_data.adc[7],
+  4, UNIT_VOLT,   0, 0, MSCBF_FLOAT, "DAC0",    &user_data.dac0,
+  4, UNIT_VOLT,   0, 0, MSCBF_FLOAT, "DAC1",    &user_data.dac1,
+  1, UNIT_BYTE,   0, 0,           0, "P1",      &user_data.p1,
+
+  1, UNIT_COUNT,  0, 0,           0, "ADCAvrg", &user_data.adc_average,
+  1, UNIT_BYTE,   0, 0,           0, "Gain0",   &user_data.gain[0],
+  1, UNIT_BYTE,   0, 0,           0, "Gain1",   &user_data.gain[1],
+  1, UNIT_BYTE,   0, 0,           0, "Gain2",   &user_data.gain[2],
+  1, UNIT_BYTE,   0, 0,           0, "Gain3",   &user_data.gain[3],
+  1, UNIT_BYTE,   0, 0,           0, "Gain4",   &user_data.gain[4],
+  1, UNIT_BYTE,   0, 0,           0, "Gain5",   &user_data.gain[5],
+  1, UNIT_BYTE,   0, 0,           0, "Gain6",   &user_data.gain[6],
+  1, UNIT_BYTE,   0, 0,           0, "Gain7",   &user_data.gain[7],
+  4, UNIT_FACTOR, 0, 0, MSCBF_FLOAT, "GainCal", &user_data.gain_cal,
+  4, UNIT_VOLT,   0, 0, MSCBF_FLOAT, "BipCal",  &user_data.bip_cal,
+  0
+};
+
+/********************************************************************\
+
+  Application specific init and inout/output routines
+
+\********************************************************************/
+
+void user_write(unsigned char index) reentrant;
+void write_gain(void) reentrant;
+
+/*---- User init function ------------------------------------------*/
+
+extern SYS_INFO sys_info;
+
+void user_init(unsigned char init)
+{
+unsigned char i;
+
+  AMX0CF = 0x00;  // select single ended analog inputs
+  ADC0CF = 0xE0;  // 16 system clocks, gain 1
+  ADC0CN = 0x80;  // enable ADC 
+
+  REF0CN = 0x03;  // enable internal reference
+  DAC0CN = 0x80;  // enable DAC0
+  DAC1CN = 0x80;  // enable DAC1
+
+  // uncomment following line for P1 push-pull mode
+  //PRT1CF = 0xFF;
+
+  /* initial EEPROM value */
+  if (init)
+    {
+    user_data.adc_average = 8;
+    for (i=0 ; i<8 ; i++)
+      user_data.gain[i] = 0;
+    user_data.gain_cal = 1;
+	  user_data.bip_cal = 0;
+
+    user_data.dac0 = 0;
+    user_data.dac1 = 0;
+    user_data.p1 = 0xff;
+    }
+
+  if (user_data.gain_cal < 0.1 || user_data.gain_cal > 10)
+    user_data.gain_cal = 1;
+
+  if (user_data.bip_cal < -1 || user_data.bip_cal > 1)
+	  user_data.bip_cal = 0;
+
+  /* write P1 and DACs */
+  user_write(8);
+  user_write(9);
+  user_write(10);
+
+  /* write gains */
+  write_gain();
+
+  // DEBUG_MODE = 1;
+}
+
+/*---- User write function -----------------------------------------*/
+
+#pragma NOAREGS
+
+void write_gain(void) reentrant
+{
+unsigned char i;
+
+  SR_STROBE = 0;
+  SR_CLOCK  = 0;
+
+  for (i=0 ; i<4 ; i++)
+    {
+    SR_DATA = ((user_data.gain[3-i] & 0x02) > 0); // first bit ext. PGA
+    SR_CLOCK = 1;
+    SR_CLOCK = 0;
+
+    SR_DATA = ((user_data.gain[3-i] & 0x01) > 0); // second bit ext. PGA
+    SR_CLOCK = 1;
+    SR_CLOCK = 0;
+    }
+  
+  for (i=0 ; i<4 ; i++)
+    {
+    SR_DATA = ((user_data.gain[7-i] & 0x02) > 0); // first bit ext. PGA
+    SR_CLOCK = 1;
+    SR_CLOCK = 0;
+
+    SR_DATA = ((user_data.gain[7-i] & 0x01) > 0); // second bit ext. PGA
+    SR_CLOCK = 1;
+    SR_CLOCK = 0;
+    }
+
+  SR_DATA   = 0;
+  SR_STROBE = 1;
+  SR_STROBE = 0;
+}
+
+void user_write(unsigned char index) reentrant
+{
+unsigned short d;
+
+  switch (index)
+    {
+    case 8:  // DAC0
+      /* assume -10V..+10V range */
+      d = ((user_data.dac0+10) / 20) * 0x1000;
+      if (d >= 0x1000)
+        d = 0x0FFF;
+
+#ifdef CPU_ADUC812
+      DAC0H = d >> 8;
+      DAC0L = d & 0xFF;
+#endif
+#ifdef CPU_C8051F000
+      DAC0L = d & 0xFF;
+      DAC0H = d >> 8;
+#endif
+      break;
+
+    case 9:  // DAC1
+      /* assume -10V..+10V range */
+      d = ((user_data.dac1+10) / 20) * 0x1000;
+      if (d >= 0x1000)
+        d = 0x0FFF;
+
+#ifdef CPU_ADUC812
+      DAC1H = d >> 8;
+      DAC1L = d & 0xFF;
+#endif
+#ifdef CPU_C8051F000
+      DAC1L = d & 0xFF;
+      DAC1H = d >> 8;
+#endif
+      break;
+
+    case 10:  // p1 
+      P1 = user_data.p1; 
+      break;
+
+    case 11:  // adc_average
+      if (user_data.adc_average > 8)
+        user_data.adc_average = 8;
+      break;
+    }
+
+  if (index > 10)
+    write_gain();
+}
+
+/*---- User read function ------------------------------------------*/
+
+unsigned char user_read(unsigned char index)
+{
+  if (index == 10)
+    user_data.p1 = P1; 
+
+  return 0;
+}
+
+/*---- User function called vid CMD_USER command -------------------*/
+
+unsigned char user_func(unsigned char *data_in,
+                        unsigned char *data_out)
+{
+  /* echo input data */
+  data_out[0] = data_in[0];
+  data_out[1] = data_in[1];
+  return 2;
+}
+
+/*---- User loop function ------------------------------------------*/
+
+void adc_read(channel, float *d)
+{
+unsigned long value;
+unsigned int  i, n;
+float gvalue;
+
+  AMX0SL = channel & 0x0F;
+  ADC0CF = 0xE0;  // 16 system clocks, gain 1
+
+  n = 1 << (user_data.adc_average+4);
+
+  value = 0;
+  for (i=0 ; i<n ; i++)
+    {
+    DISABLE_INTERRUPTS;
+
+    ADCINT = 0;
+    ADBUSY = 1;
+    while (!ADCINT);  // wait until conversion ready, does NOT work with ADBUSY!
+
+    ENABLE_INTERRUPTS;
+
+    value += (ADC0L | (ADC0H << 8));
+    yield();
+    }
+
+  if (user_data.adc_average)
+    value >>= (user_data.adc_average);
+
+  /* convert to volts */
+  gvalue = value / 65536.0 * 2.5;
+
+  /* subtract 1V for bipolar mode */
+  if (user_data.gain[channel] & 0x04)
+    gvalue = gvalue - 1;
+
+  /* external voltage divider */
+  gvalue *= 10 * user_data.gain_cal;
+
+  /* correct for bipolar offset */
+  if (user_data.gain[channel] & 0x04)
+    gvalue += user_data.bip_cal;
+
+  /* external PGA */
+  switch (user_data.gain[channel] & 0x03)
+    {
+    case 0x00: gvalue /= 1.0; break;
+    case 0x01: gvalue /= 10.0; break;
+    case 0x02: gvalue /= 100.0; break;
+    case 0x03: gvalue /= 1000.0; break;
+    }
+
+  DISABLE_INTERRUPTS;
+  *d = gvalue;
+  ENABLE_INTERRUPTS;
+}
+
+void user_loop(void)
+{
+static unsigned char i = 0;
+
+  adc_read(i, &user_data.adc[i]);
+  i = (i+1) % 8;
+}
+
