f2fc44a65cfc7bbb4eb51bef041f79646c215c59
Stefan Ritt <stefan.ritt@psi.ch>
2003-09-29 07:59:46 +0000

    *** empty log message ***
    SVN revision: 1943

--- include/hardware.h
+++ include/hardware.h
@@ -7,6 +7,9 @@
                 hardware.
 
   $Log$
+  Revision 1.3  2003/09/29 07:59:46  midas
+  *** empty log message ***
+
   Revision 1.2  1998/10/12 12:18:59  midas
   Added Log tag in header
 
@@ -19,7 +22,7 @@
 #define TID_LRS1882    (1<<8)  /* LeCroy 1882 fastbus ADC */
 #define TID_LRS1877    (2<<8)  /* LeCroy 1877 fastbus TDC */
 #define TID_PCOS3      (3<<8)  /* LeCroy PCOS 3 wire chambers */
-
+#define TID_LRS1875    (4<<8)  /* LeCroy 1875 fastbus TDC */
 /* Hardware data records */
 
 typedef struct {
@@ -49,3 +52,12 @@ typedef struct {
   WORD geo_addr : 5; 
 } LRS1877_HEADER;
 
+typedef struct {
+  WORD data     : 12;
+  WORD          : 4;
+  WORD channel  : 6;
+  WORD          : 1;
+  WORD range    : 1;
+  WORD event    : 3;
+  WORD geo_addr : 5;
+} LRS1875_DATA;
