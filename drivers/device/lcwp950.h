22dcdc95dc619be74c7039cdeedc5e172f3a79de
Matthias Schneebeli <matthias.schneebeli@psi.ch>
2004-07-16 08:20:34 +0000

    Added timeout to scope_wait()
    SVN revision: 2255

--- drivers/device/lcwp950.h
+++ drivers/device/lcwp950.h
@@ -7,8 +7,8 @@
                 header file
 
   $Log$
-  Revision 1.2  2004/01/08 08:40:08  midas
-  Implemented standard indentation
+  Revision 1.3  2004/07/16 08:20:34  schneebeli
+  Added timeout to scope_wait()
 
   Revision 1.1  2002/01/14 16:49:53  midas
   Initial revisioin
@@ -24,5 +24,5 @@
 int scope_open(char *addr, int channel);
 void scope_close(int fh);
 void scope_arm();
-void scope_wait(int fh);
+int scope_wait(int fh, unsigned int timeout);
 int scope_read(int fh, double x[SCOPE_MEM_SIZE], double y[SCOPE_MEM_SIZE]);
