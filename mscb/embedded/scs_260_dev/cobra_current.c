79fcdbedb068bc0421cfd81efda526015f0dde5a
Stefan Ritt <stefan.ritt@psi.ch>
2005-03-17 14:19:38 +0000

    Added TCP code
    SVN revision: 2536

--- mscb/embedded/scs_260_dev/cobra_current.c
+++ mscb/embedded/scs_260_dev/cobra_current.c
@@ -8,6 +8,9 @@
                 in subnet of the experimental hall.
 
   $Log$
+  Revision 1.2  2005/03/17 14:19:38  ritt
+  Added TCP code
+
   Revision 1.1  2005/03/16 14:20:34  ritt
   Initial revision
 
@@ -22,11 +25,14 @@
 #include <netinet/in.h>
 #endif
 
+#define UDP // Remove this line for TCP code
+
 main()
 {
    int sock, size, n;
-   char str[80];
+   char str[80], host_name[80];
    struct sockaddr_in myaddr, addr;
+   struct hostent *phe;
 
 #if defined( _MSC_VER )
    {
@@ -38,6 +44,7 @@ main()
    }
 #endif
 
+#ifdef UDP // UDP code ---------------------------------------
    sock = socket(AF_INET, SOCK_DGRAM, 0);
 
    /* bind to port 1178 to receive cobra current broadcasts */
@@ -56,5 +63,44 @@ main()
       printf(str);
    } while (1);
 
-   printf("Hello\n");
+#else // TCP code --------------------------------------------
+
+   strcpy(host_name, "MSCB001");
+
+   sock = socket(AF_INET, SOCK_STREAM, 0);
+
+   /* let OS choose any port number */
+   memset(&myaddr, 0, sizeof(myaddr));
+   myaddr.sin_family = AF_INET;
+   myaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+   myaddr.sin_port = 0;
+   bind(sock, (void *) &myaddr, sizeof(myaddr));
+
+   /* connect to remote node */
+   memset(&addr, 0, sizeof(addr));
+   addr.sin_family = AF_INET;
+   addr.sin_addr.s_addr = 0;
+   addr.sin_port = htons(23); // telnet port
+
+   phe = gethostbyname(host_name);
+   if (phe == NULL) {
+      printf("Cannot resolve host name %s\n", host_name);
+      return -1;
+   }
+   memcpy((char *) &(addr.sin_addr), phe->h_addr, phe->h_length);
+
+   if (connect(sock, (void *) &addr, sizeof(addr)) != 0) {
+      printf("Cannot connect to %s\n", host_name);
+      return -1;
+   }
+
+   do {
+      memset(str, 0, sizeof(str));
+      n = recv(sock, str, sizeof(str), 0);
+
+      /* just print string */
+      printf(str);
+   } while (1);
+
+#endif
 }
