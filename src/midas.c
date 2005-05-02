659e91be8fa65d3691ad162186e935d31d5296df
Stefan Ritt <stefan.ritt@psi.ch>
2005-05-02 10:26:21 +0000

    *** empty log message ***
    SVN revision: 2621

--- src/midas.c
+++ src/midas.c
@@ -6,6 +6,9 @@
   Contents:     MIDAS main library funcitons
 
   $Log$
+  Revision 1.231  2005/05/02 10:26:21  ritt
+  *** empty log message ***
+
   Revision 1.230  2005/02/16 14:59:05  ritt
   Implemented >2GB file support for midas.log
 
@@ -962,73 +965,6 @@ void dbg_free(void *adr, char *file, int line)
    fclose(f);
 
 }
-
-#ifndef HAVE_STRLCPY
-/*---- strlcpy and strlcat to avoid buffer overflow ----------------*/
-
-/*
- * Copy src to string dst of size siz.  At most siz-1 characters
- * will be copied.  Always NUL terminates (unless size == 0).
- * Returns strlen(src); if retval >= siz, truncation occurred.
- */
-INT strlcpy(char *dst, const char *src, INT size)
-{
-   char *d = dst;
-   const char *s = src;
-   INT n = size;
-
-   /* Copy as many bytes as will fit */
-   if (n != 0 && --n != 0) {
-      do {
-         if ((*d++ = *s++) == 0)
-            break;
-      } while (--n != 0);
-   }
-
-   /* Not enough room in dst, add NUL and traverse rest of src */
-   if (n == 0) {
-      if (size != 0)
-         *d = '\0';             /* NUL-terminate dst */
-      while (*s++);
-   }
-
-   return (s - src - 1);        /* count does not include NUL */
-}
-
-/*
- * Appends src to string dst of size siz (unlike strncat, siz is the
- * full size of dst, not space left).  At most siz-1 characters
- * will be copied.  Always NUL terminates (unless size <= strlen(dst)).
- * Returns strlen(src) + MIN(size, strlen(initial dst)).
- * If retval >= size, truncation occurred.
- */
-INT strlcat(char *dst, const char *src, INT size)
-{
-   char *d = dst;
-   const char *s = src;
-   INT n = size;
-   INT dlen;
-
-   /* Find the end of dst and adjust bytes left but don't go past end */
-   while (n-- != 0 && *d != '\0')
-      d++;
-   dlen = d - dst;
-   n = size - dlen;
-
-   if (n == 0)
-      return (dlen + strlen(s));
-   while (*s != '\0') {
-      if (n != 1) {
-         *d++ = *s;
-         n--;
-      }
-      s++;
-   }
-   *d = '\0';
-
-   return (dlen + (s - src));   /* count does not include NUL */
-}
-#endif
 
 /********************************************************************\
 *                                                                    *
