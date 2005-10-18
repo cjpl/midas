e491fb2c2977038b21ebe78c96427320e1ef50f7
Stefan Ritt <stefan.ritt@psi.ch>
2005-10-18 11:54:15 +0000

    Initial revision
    SVN revision: 2777

---
+++ drivers/device/mscbhvr.h
@@ -0,0 +1,12 @@
+/********************************************************************\
+
+  Name:         mscbhvr.h
+  Created by:   Stefan Ritt
+
+  Contents:     Device driver function declarations for MSCB-HVR device
+
+  $Id: nulldev.h 2753 2005-10-07 14:55:31Z ritt $
+
+\********************************************************************/
+
+INT mscbhvr(INT cmd, ...);
