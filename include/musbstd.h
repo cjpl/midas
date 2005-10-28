97dd5d2ee701f17789e672342df213ffe5bc0f7a
Stefan Ritt <stefan.ritt@psi.ch>
2005-10-28 15:35:56 +0000

    Added error codes and MUSB_INTERFACE
    SVN revision: 2836

--- include/musbstd.h
+++ include/musbstd.h
@@ -1,19 +1,62 @@
 /********************************************************************\
 
   Name:         musbstd.h
-  Created by:   Konstantin Olchanski
+  Created by:   Konstantin Olchanski, Stefan Ritt
 
   Contents:     Midas USB access
 
-  $Id:$
+  $Id$
 
 \********************************************************************/
 
+#ifndef MUSBSTD_H
+#define MUSBSTD_H
 
-void* musb_open(int vendor,int product,int instance,int configuration,int interface);
-int musb_close(void* handle);
-int musb_write(void* handle,int endpoint,const void *buf,int count,int timeout_ms);
-int musb_read(void* handle,int endpoint,void *buf,int count,int timeout_ms);
-int musb_reset(void*handle);
+#if defined(_MSC_VER)
 
-/* end */
+#include <windows.h>
+
+typedef struct {
+   HANDLE rhandle;
+   HANDLE whandle;
+} MUSB_INTERFACE;
+
+#elif defined(HAVE_LIBUSB)
+
+typedef struct {
+   usb_dev_handle *dev;
+   int usbinterface;
+} MUSB_INTERFACE;
+
+#elif defined(OS_DARWIN)
+
+typedef struct {
+   IOUSBInterfaceInterface **handle;
+} MUSB_INTERFACE;
+
+#endif
+
+/*---- status codes ------------------------------------------------*/
+
+#define MUSB_SUCCESS                  1
+#define MUSB_NOT_FOUND                2
+#define MUSB_INVALID_PARAM            3
+#define MUSB_NO_MEM                   4
+#define MUSB_ACCESS_ERROR             5
+
+/* make functions callable from a C++ program */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int musb_open(MUSB_INTERFACE **musb_interface, int vendor, int product, int instance, int configuration, int usbinterface);
+int musb_close(MUSB_INTERFACE *musb_interface);
+int musb_write(MUSB_INTERFACE *musb_interface,int endpoint,const void *buf,int count,int timeout_ms);
+int musb_read(MUSB_INTERFACE *musb_interface,int endpoint,void *buf,int count,int timeout_ms);
+int musb_reset(MUSB_INTERFACE *musb_interface);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // MUSBSTD_H
