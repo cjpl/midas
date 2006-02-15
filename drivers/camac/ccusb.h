da70b41312fdeec51de194b8696a24dfee8a9f95
Pierre Amaudruz <amaudruz@triumf.ca>
2006-02-15 18:27:42 +0000

    - Adjust to musbstd calls (linux test)
    SVN revision: 3036

--- drivers/camac/ccusb.h
+++ drivers/camac/ccusb.h
@@ -1,19 +1,32 @@
+/********************************************************************\
+  Name:         ccusb.h
+  Created by:   Konstantin Olchanski & P.-A. Amaudruz
+
+  Contents:     Wiener CAMAC Controller USB 
+                Using musbstd.h
+\********************************************************************/
 // CC-USB ccusb.h
+
+#include "musbstd.h"
+
+#ifndef CCUSB_H
+#define CCUSB_H
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 int   ccusb_init();
-void* ccusb_getCrateHandle(int crate);
-int   ccusb_flush(void*handle);
-int   ccusb_readReg(void*handle,int ireg);
-int   ccusb_writeReg(void*handle,int ireg,int value);
-int   ccusb_reset(void*handle);
-int   ccusb_status(void*handle);
-int   ccusb_naf(void*handle,int n,int a,int f,int d24,int data);
+MUSB_INTERFACE* ccusb_getCrateStruct(int crate);
+int   ccusb_flush(MUSB_INTERFACE *);
+int   ccusb_readReg(MUSB_INTERFACE *, int ireg);
+int   ccusb_writeReg(MUSB_INTERFACE *, int ireg, int value);
+int   ccusb_reset(MUSB_INTERFACE *);
+int   ccusb_status(MUSB_INTERFACE *);
+int   ccusb_naf(MUSB_INTERFACE *, int n, int a, int f, int d24, int data);
 
 #ifdef __cplusplus
 }
 #endif
-// end
+#endif // CCUSB_H
+//end
