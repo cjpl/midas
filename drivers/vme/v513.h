6700dfc9b90ca422a702c9470962ec199a3e0e56
olchanski <olchanski@050218f5-8902-0410-8d0e-8a15d521e4f2>
2006-05-08 00:19:25 +0000

    Drivers for CAEN VME modules written for CERN-ALPHA: v513 is a NIM I/O register v560 is a 32-bit scaler
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/midas/trunk@3088 050218f5-8902-0410-8d0e-8a15d521e4f2

---
+++ drivers/vme/v513.h
@@ -0,0 +1,37 @@
+/*********************************************************************
+
+  Name:         v513.h
+  Created by:   K.Olchanski
+
+  Contents:     CAEN V513 16-channel NIM I/O register
+                
+  $Log$
+*********************************************************************/
+
+#ifndef  V513_INCLUDE_H
+#define  V513_INCLUDE_H
+
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include "mvmestd.h"
+
+#define V513_CHANMODE_OUTPUT    0
+#define V513_CHANMODE_INPUT     1
+#define V513_CHANMODE_NEG       0
+#define V513_CHANMODE_POS       2
+#define V513_CHANMODE_IGLITCHED 0
+#define V513_CHANMODE_INORMAL   4
+#define V513_CHANMODE_TRANSP    0
+#define V513_CHANMODE_EXTSTROBE 8
+
+uint16_t v513_RegisterRead(MVME_INTERFACE *mvme, DWORD base, int offset);
+void     v513_RegisterWrite(MVME_INTERFACE *mvme, DWORD base, int offset, uint16_t value);
+uint16_t v513_Read(MVME_INTERFACE *mvme, DWORD base);
+void     v513_Write(MVME_INTERFACE *mvme, DWORD base, uint16_t data);
+void     v513_Reset(MVME_INTERFACE *mvme, DWORD base);
+void     v513_Status(MVME_INTERFACE *mvme, DWORD base);
+void     v513_SetChannelMode(MVME_INTERFACE *mvme, DWORD base, int channel, int mode); // mode are V513_CHANMODE_XXX bits
+int      v513_GetChannelMode(MVME_INTERFACE *mvme, DWORD base, int channel);
+
+#endif // V513_INCLUDE_H
---
+++ drivers/vme/v560.c
@@ -0,0 +1,68 @@
+/*********************************************************************
+
+  Name:         v560.c
+  Created by:   K.Olchanski
+
+  Contents:     CAEN V560 16-channel 32-bit scaler
+                
+  $Log$
+*********************************************************************/
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include "v560.h"
+#include "mvmestd.h"
+
+/*****************************************************************/
+/*
+Read V560 register value
+*/
+static uint16_t regRead(MVME_INTERFACE *mvme, DWORD base, int offset)
+{
+  mvme_set_am(mvme, MVME_AM_A24);
+  mvme_set_dmode(mvme, MVME_DMODE_D16);
+  return mvme_read_value(mvme, base + offset);
+}
+
+/*****************************************************************/
+/*
+Write V560 register value
+*/
+static void regWrite(MVME_INTERFACE *mvme, DWORD base, int offset, uint16_t value)
+{
+  mvme_set_am(mvme, MVME_AM_A24);
+  mvme_set_dmode(mvme, MVME_DMODE_D16);
+  mvme_write_value(mvme, base + offset, value);
+}
+
+uint16_t v560_RegisterRead(MVME_INTERFACE *mvme, DWORD base, int offset)
+{
+  return regRead(mvme,base,offset);
+}
+
+void     v560_RegisterWrite(MVME_INTERFACE *mvme, DWORD base, int offset, uint16_t value)
+{
+  regWrite(mvme,base,offset,value);
+}
+
+void v560_Read(MVME_INTERFACE *mvme, DWORD base, uint32_t data[16])
+{
+  mvme_set_am(mvme, MVME_AM_A24);
+  mvme_set_dmode(mvme, MVME_DMODE_D32);
+  mvme_read(mvme, data, base + 0x10, 16*4);
+}
+
+/*****************************************************************/
+void v560_Reset(MVME_INTERFACE *mvme, DWORD base)
+{
+  regRead(mvme,base,0x54); // VME VETO reset
+  regRead(mvme,base,0x50); // scalers clear, vme interrupt clear and disable
+}
+
+/*****************************************************************/
+void  v560_Status(MVME_INTERFACE *mvme, DWORD base)
+{
+  printf("CAEN V560 at A24 0x%x: version 0x%x, type 0x%x, code 0x%x, scaler status: 0x%x\n", (int)base,regRead(mvme,base,0xFE),regRead(mvme,base,0xFC),regRead(mvme,base,0xFA),regRead(mvme,base,0x58));
+}
+
+//end
