0defa1b3423a8e2ab1c522bf255dbde23ce8e741
amaudruz <amaudruz@050218f5-8902-0410-8d0e-8a15d521e4f2>
2006-08-04 23:34:08 +0000

    Initial class for the CAEN 2527
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/midas/trunk@3250 050218f5-8902-0410-8d0e-8a15d521e4f2

---
+++ drivers/device/dd_sy2527.h
@@ -0,0 +1,12 @@
+/********************************************************************\
+
+  Name:         dd_sy2527.h
+  Created by:   Stefan Ritt
+
+  Contents:     Device driver function declarations for NULL device
+
+  $Id: nulldev.h 2753 2005-10-07 14:55:31Z ritt $
+
+\********************************************************************/
+
+INT dd_sy2527(INT cmd, ...);
