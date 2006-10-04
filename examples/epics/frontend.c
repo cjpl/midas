63cf77e174bafbf508eafc630f6352e338283178
Pierre Amaudruz <amaudruz@triumf.ca>
2006-10-04 06:39:42 +0000

    add watchdog control
    SVN revision: 3351

--- examples/epics/frontend.c
+++ examples/epics/frontend.c
@@ -18,7 +18,7 @@
 /*-- Globals -------------------------------------------------------*/
 
 /* The frontend name (client name) as seen by other MIDAS clients   */
-char *frontend_name = "BL Frontend";
+char *frontend_name = "feEpics";
 /* The frontend file name, don't change it */
 char *frontend_file_name = __FILE__;
 
@@ -26,7 +26,7 @@ char *frontend_file_name = __FILE__;
 BOOL frontend_call_loop = TRUE;
 
 /* a frontend status page is displayed with this frequency in ms    */
-INT display_period = 000;
+INT display_period = 2000;
 
 /* maximum event size for fragmented events (EQ_FRAGMENTED) */
 INT max_event_size_frag = 5 * 1024 * 1024;
@@ -40,7 +40,7 @@ INT event_buffer_size = 10 * 10000;
 /*-- Equipment list ------------------------------------------------*/
 
 /* 
-The following statement allocates 10 channels for the beamline
+The following statement allocates 20 channels for the beamline
 control through the epics channel access device driver. The 
 EPICS channel names are stored under 
    
@@ -49,13 +49,13 @@ EPICS channel names are stored under
 while the channel names as the midas slow control sees them are
 under 
 
-  /Equipment/Bemaline/Settings/Names
+  /Equipment/Beamline/Settings/Names
 
 An example set of channel names is saved in the triumf.odb file
 in this directory and can be loaded into the ODB with the odbedit
 command
 
-  load triumf.odb
+  load beamline.xml
 
 before the frontend is started. The CMD_SET_LABEL statement 
 actually defines who determines the label name. If this flag is
@@ -127,33 +127,45 @@ INT frontend_exit()
 }
 
 /*-- Frontend Loop -------------------------------------------------*/
-
+/* Issue a watchdog counter every second for the Epics world
+   for R/W access control.
+   This counter will appear in the measured variable under index 19.
+*/
 INT frontend_loop()
 {
-  static HNDLE hDB, hWatch=0;
+  int status, size;
+  static HNDLE hDB, hWatch=0, hRespond;
   static DWORD watchdog_time=0;
-  static float  dog=0.f;
-   
+  static float  dog=0.f, cat=0.f;
+  
   /* slow down frontend not to eat all CPU cycles */
   /* ss_sleep(50); */
   cm_yield(50); /* 15Feb05 */
-   
+  
   if (ss_time() - watchdog_time > 1)
-  {
-    watchdog_time = ss_time();
-    if (!hWatch)
     {
-      cm_get_experiment_database(&hDB, NULL);
-      if (db_find_key(hDB, 0, "/equipment/Beamline/variables/demand", &hWatch) != DB_SUCCESS)
-      {
-       cm_msg(MERROR, "frontend_loop", "key not found");
-       return FE_ERR_HW;
+      watchdog_time = ss_time();
+      if (!hWatch)
+	{
+	  cm_get_experiment_database(&hDB, NULL);
+	  status = db_find_key(hDB, 0, "/equipment/Beamline/variables/demand", &hWatch);
+	  status = db_find_key(hDB, 0, "/equipment/Beamline/variables/measured", &hRespond);
+	  if (status != DB_SUCCESS) {
+	    cm_msg(MERROR, "frontend_loop", "key not found");
+	    return FE_ERR_HW;
+	  }
+	}
+      if (hWatch) {
+	/* Check if Epics alive */
+	size = sizeof(float);
+	db_get_data_index(hDB, hRespond, &cat, &size, 19, TID_FLOAT);
+	if (abs(cat - dog) > 10.f)
+	  cm_msg(MINFO,"feEpics","R/W Access to Epics is in jeopardy!");
+	
+	db_set_data_index(hDB, hWatch, &dog, sizeof(float), 19, TID_FLOAT);
       }
+      if (!((INT)++dog % 100)) dog = 0.f;
     }
-    if (hWatch) 
-      db_set_data_index(hDB, hWatch, &dog, sizeof(float), 19, TID_FLOAT);
-    if (!((INT)++dog % 100)) dog = 0.f;
-  }
   return CM_SUCCESS;
 }
 
@@ -178,7 +190,7 @@ INT pause_run(INT run_number, char *error)
    return CM_SUCCESS;
 }
 
-/*-- Resuem Run ----------------------------------------------------*/
+/*-- Resume Run ----------------------------------------------------*/
 
 INT resume_run(INT run_number, char *error)
 {
