4b90d9029ac259a89b61ea17d08e92898576906f
Stefan Ritt <stefan.ritt@psi.ch>
2006-10-09 10:07:40 +0000

    Added fan RPM measurement
    SVN revision: 3358

--- mscb/embedded/meg_splt_bp.c
+++ mscb/embedded/meg_splt_bp.c
@@ -32,6 +32,10 @@ sbit DAC_SCK  = P3 ^ 1;
 sbit DAC_NCS  = P3 ^ 0;
 sbit ENABLE   = P0 ^ 7;
 
+sbit FAN1     = P2 ^ 7;
+sbit FAN2     = P2 ^ 6;
+sbit FAN3     = P2 ^ 5;
+
 /*---- Define variable parameters returned to CMD_GET_INFO command ----*/
 
 /* data buffer (mirrored in EEPROM) */
@@ -39,6 +43,7 @@ sbit ENABLE   = P0 ^ 7;
 struct {
    unsigned char enable;
    float dac;
+   unsigned int fan1, fan2, fan3;
    float offset;
    float gain;
 } xdata user_data;
@@ -47,6 +52,9 @@ MSCB_INFO_VAR code vars[] = {
 
    { 1, UNIT_BOOLEAN, 0, 0, 0,                          "Enable",  &user_data.enable },
    { 4, UNIT_VOLT,    0, 0, MSCBF_FLOAT,                "DAC",     &user_data.dac    },
+   { 2, UNIT_RPM,     0, 0, 0,                          "Fan1",    &user_data.fan1   },
+   { 2, UNIT_RPM,     0, 0, 0,                          "Fan2",    &user_data.fan2   },
+   { 2, UNIT_RPM,     0, 0, 0,                          "Fan3",    &user_data.fan3   },
    { 4, UNIT_VOLT,    0, 0, MSCBF_FLOAT | MSCBF_HIDDEN, "Offset",  &user_data.offset },
    { 4, UNIT_FACTOR,  0, 0, MSCBF_FLOAT | MSCBF_HIDDEN, "Gain",    &user_data.gain   },
 
@@ -72,21 +80,24 @@ void user_init(unsigned char init)
 
 
    /* open drain(*) /push-pull: 
-      P0.0 TX1      P3.0 SSYNC
-      P0.1*RX1      P3.1 SCLK
-      P0.2 TX2      P3.2 SOUT
-      P0.3*RX2      P3.3 LED
-                                  
-      P0.4          P3.4*TRIN0
-      P0.5          P3.5*TRIN1
-      P0.6          P3.6*TRIN2
-      P0.7 ENABLE   P3.7*TRIN3
+      P0.0 TX1      P2.0          P3.0 SSYNC
+      P0.1*RX1      P2.1          P3.1 SCLK 
+      P0.2 TX2      P2.2          P3.2 SOUT 
+      P0.3*RX2      P2.3          P3.3 LED  
+                                            
+      P0.4          P2.4 *PIC     P3.4*TRIN0
+      P0.5          P2.5 *FAN1    P3.5*TRIN1
+      P0.6          P2.6 *FAN1    P3.6*TRIN2
+      P0.7 ENABLE   P2.7 *FAN1    P3.7*TRIN3
     */
    SFRPAGE = CONFIG_PAGE;
    P0MDOUT = 0xF5;
    P1MDOUT = 0xFF;
-   P2MDOUT = 0xFF;
+   P2MDOUT = 0xF0;
    P3MDOUT = 0x0F;
+
+   P2 = 0xFF;
+   P3 = 0xFF;
 
    /* initial EEPROM value */
    if (init) {
@@ -169,6 +180,37 @@ void write_dac(float value) reentrant
 
 /*---- User loop function ------------------------------------------*/
 
+unsigned short fan1, fan2, fan3;
+unsigned long fan_last = 0;
+bit old_fan1, old_fan2, old_fan3;
+
 void user_loop(void)
 {
+   if (FAN1 && !old_fan1)
+      fan1++;
+
+   if (FAN2 && !old_fan2)
+      fan2++;
+
+   if (FAN3 && !old_fan3)
+      fan3++;
+
+   old_fan1 = FAN1;
+   old_fan2 = FAN2;
+   old_fan3 = FAN3;
+
+   if (time() - fan_last >= 100) {
+      DISABLE_INTERRUPTS;
+      user_data.fan1 = fan1*60;
+      user_data.fan2 = fan2*60;
+      user_data.fan3 = fan3*60;
+      ENABLE_INTERRUPTS;
+
+      if (fan1 > 0 && fan2 > 0 && fan3 > 0)
+         led_blink(0, 1, 50);
+
+      fan1 = fan2 = fan3 = 0;
+      fan_last = time();
+   }
+
 }
