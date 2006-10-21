5534487f8d7be74f9ef1b036a3d6216b25e5c8a8
amaudruz <amaudruz@050218f5-8902-0410-8d0e-8a15d521e4f2>
2006-10-21 05:38:03 +0000

    Initial version for T2K Fine Grid Detector Frontend SC.
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/midas/trunk@3387 050218f5-8902-0410-8d0e-8a15d521e4f2

---
+++ drivers/device/dd_mscbfgd.h
@@ -0,0 +1,12 @@
+/********************************************************************\
+
+  Name:         dd_mscbfgd.h
+  Created by:   Pierre-André Amaudruz
+
+  Contents:     Device driver function declarations for MSCB-FGD device
+
+  $Id: dd_mscbfgd.h amaudruz $
+
+\********************************************************************/
+
+INT mscbfgd(INT cmd, ...);
