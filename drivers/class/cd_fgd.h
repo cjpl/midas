9a5a99f57c94989b7b205907551d062c8067b512
Pierre Amaudruz <amaudruz@triumf.ca>
2006-10-21 05:28:24 +0000

    - initial version for T2k-FGD frontend control
    SVN revision: 3385

---
+++ drivers/class/cd_fgd.h
@@ -0,0 +1,14 @@
+/********************************************************************\
+
+  Name:         cd_fgd.h
+  Created by:   Pierre-Andre Amaudruz
+
+  Contents:     FGD / T2K Class Driver header file
+
+  $Id:$
+
+\********************************************************************/
+
+/* class driver routines */
+INT cd_fgd(INT cmd, PEQUIPMENT pequipment);
+INT cd_fgd_read(char *pevent, int);
