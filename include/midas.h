a2de45df45c576687dc0c019e0ae962cba840834
ritt <ritt@050218f5-8902-0410-8d0e-8a15d521e4f2>
2006-11-24 16:33:56 +0000

    Increased max. event size to 4MB, added some comments
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/midas/trunk@3411 050218f5-8902-0410-8d0e-8a15d521e4f2

--- include/midas.h
+++ include/midas.h
@@ -214,7 +214,7 @@ typedef INT midas_thread_t;
 #define OPT_TCP_SIZE           8192          /**< optimal TCP buffer size     */
 #define NET_UDP_SIZE           8192          /**< maximum UDP transfer        */
 
-#define MAX_EVENT_SIZE         0x200000      /**< maximum event size 2MB      */
+#define MAX_EVENT_SIZE         0x400000      /**< maximum event size 4MB      */
 #define EVENT_BUFFER_NAME      "SYSTEM"      /**< buffer name for commands    */
 #define DEFAULT_ODB_SIZE       0x100000      /**< online database 1M          */
 
@@ -676,7 +676,7 @@ Slow control device driver commands */
 #define CMD_MISC_LAST                 9 /* update this if you add new commands */
 
 #define CMD_SET_FIRST                CMD_MISC_LAST+1 /* set commands */
-#define CMD_SET                      CMD_SET_FIRST 
+#define CMD_SET                      CMD_SET_FIRST   // = 10
 #define CMD_SET_VOLTAGE_LIMIT        CMD_SET_FIRST+1
 #define CMD_SET_CURRENT_LIMIT        CMD_SET_FIRST+2
 #define CMD_SET_RAMPUP               CMD_SET_FIRST+3
@@ -685,12 +685,12 @@ Slow control device driver commands */
 #define CMD_SET_LAST                 CMD_SET_FIRST+5 /* update this if you add new commands */
 
 #define CMD_GET_FIRST                CMD_SET_LAST+1  /* multithreaded get commands */
-#define CMD_GET                      CMD_GET_FIRST
+#define CMD_GET                      CMD_GET_FIRST   // = 16
 #define CMD_GET_CURRENT              CMD_GET_FIRST+1
 #define CMD_GET_LAST                 CMD_GET_FIRST+2 /* update this if you add new commands ! */
 
 #define CMD_GET_DIRECT               CMD_GET_LAST+1  /* direct get commands */
-#define CMD_GET_DEMAND               CMD_GET_DIRECT
+#define CMD_GET_DEMAND               CMD_GET_DIRECT  // = 19
 #define CMD_GET_VOLTAGE_LIMIT        CMD_GET_DIRECT+1
 #define CMD_GET_CURRENT_LIMIT        CMD_GET_DIRECT+2
 #define CMD_GET_RAMPUP               CMD_GET_DIRECT+3
