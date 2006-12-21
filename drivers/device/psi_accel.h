e1c9bce2415919f34bbe4169161c8993dec77e91
Stefan Ritt <stefan.ritt@psi.ch>
2006-12-21 14:42:23 +0000

    Initial revision
    SVN revision: 3481

---
+++ drivers/device/psi_accel.h
@@ -0,0 +1,12 @@
+/********************************************************************\
+
+  Name:         psi_accel.h
+  Created by:   Stefan Ritt
+
+  Contents:     Header file for psi_accel.c device driver
+
+  $Id: psi_beamblocker.h 3158 2006-06-22 08:48:47Z ritt $
+
+\********************************************************************/
+
+INT psi_accel(INT cmd, ...);
