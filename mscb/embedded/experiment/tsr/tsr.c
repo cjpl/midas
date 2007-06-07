045ba817f7774367a6b23321db7ed741e8258809
amaudruz <amaudruz@050218f5-8902-0410-8d0e-8a15d521e4f2>
2007-06-07 18:50:58 +0000

    Temperature Sensor ReadBack with SST Protocol
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/midas/trunk@3699 050218f5-8902-0410-8d0e-8a15d521e4f2

---
+++ mscb/embedded/experiment/tsr/tsr.c
@@ -0,0 +1,180 @@
+/********************************************************************\
+  Name:         fgd_008_tsr.c
+  Created by:   Brian Lee						    May/11/2007
+
+
+  Contents:     Application specific (user) part of
+                Midas Slow Control Bus protocol
+                for FGD-008-TSR (Temperature Sensor Array)
+				testing board
+\********************************************************************/
+//  need to have FGD_008_TSR defined.
+
+#include <stdio.h>
+#include "../../mscbemb.h"
+#include "ADT7486A_tsensor.h"
+#include "tsr.h"
+extern bit FREEZE_MODE;
+extern bit DEBUG_MODE;
+
+/* declare number of sub-addresses to framework */
+unsigned char idata _n_sub_addr = N_HV_CHN;
+
+char code node_name[] = "TSR";
+
+/* AD7718 pins */
+sbit ADC_NRES = P1 ^ 0;         // !Reset
+sbit ADC_SCLK = P0 ^ 6;         // Serial Clock
+sbit ADC_NCS  = P1 ^ 5;         // !Chip select
+sbit ADC_NRDY = P1 ^ 3;         // !Ready
+sbit ADC_DOUT = P1 ^ 0;         // Data out
+sbit ADC_DIN  = P0 ^ 7;         // Data in
+
+/* LTC2600 pins */
+sbit DAC_NCS  = P1 ^ 2;         // !Chip select
+sbit DAC_SCK  = P0 ^ 6;         // Serial Clock
+sbit DAC_CLR  = P1 ^ 7;         // Clear
+sbit DAC_DIN  = P0 ^ 7;         // Data in
+
+/* Charge Pump */
+sbit INT_BIAS = P1 ^ 2;         // Internal bias path enable
+sbit EXT_BIAS = P1 ^ 3;         // External bias path enable
+
+/* Cross Bar Related */
+//sbit SMBus_XBR_Enable = XBR0 ^ 2;
+//sbit CP0_XBR_Enable = XBR0 ^ 4;
+//sbit CP0_Fn_Enable = CPT0CN ^ 7;
+//sbit CP0A_XBR_Enable = XBR0 ^ 5;
+
+unsigned char idata chn_bits[N_HV_CHN];
+float         xdata u_actual[N_HV_CHN];
+unsigned long xdata t_ramp[N_HV_CHN];
+
+bit trip_reset;
+bit once = 1;
+
+struct user_data_type xdata user_data[N_HV_CHN];
+
+MSCB_INFO_VAR code vars[] = {
+   //Main variables (with just "read" call)
+   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT, "ExtTemp",  &user_data[0].external_temp, //0
+   //Hidden variables (wth "read all" call)
+   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT | MSCBF_HIDDEN, "TempOS",  &user_data[0].external_tempOffset, //1(hidden)
+   4, UNIT_CELSIUS,         0, 0, MSCBF_FLOAT | MSCBF_HIDDEN, "IntTemp",  &user_data[0].internal_temp, //2(hidden)
+   0  // terminating zero, this is needed for the completion of user_data structure
+};
+
+
+MSCB_INFO_VAR *variables = vars;
+
+/********************************************************************\
+
+  Application specific init and inout/output routines
+
+\********************************************************************/
+
+/*---- User init function ------------------------------------------*/
+
+extern SYS_INFO sys_info;
+
+void user_init(unsigned char init)
+{
+   unsigned char  i;
+
+   /* Set outputs' open-drain/push-pull */
+   P0MDOUT = P0MDOUT | 0x08;
+
+   RS485_ENABLE = 0; //get C8051 to transmit data out
+
+   PCA0MD   = 0x8;  // Sysclk (24.6MHz)
+    
+   /* initial nonzero EEPROM values */
+   if (init) {
+
+      for (i=0 ; i<N_HV_CHN ; i++) {
+         user_data[i].internal_temp = 0.0;
+		 user_data[i].external_temp = 0.0;
+		 user_data[i].external_tempOffset = 0.0;
+      }
+   }
+
+   // force update
+   for (i=0 ; i<N_HV_CHN ; i++)
+      chn_bits[i] = DEMAND_CHANGED;
+
+   // set normal LED mode (non-inverted mode)
+   for (i=0 ; i<N_HV_CHN ; i++) {
+      led_mode(i+2, 0);
+   }
+
+	/* Cross Bar Settings */
+	//if the RS485 Communication is not working, it's 90% because of
+	//crossbar settings!
+
+	P0SKIP = 0x0C; //skip P0 ^ 2 and P0 ^ 3
+				   //P0 ^ 4 and P0 ^ 5 are automatically assigned to be TX0 and RX0
+				   //see below where crossbar routes the ports for TX0 and RX0
+
+	XBR0 = 0x35;	//route SMBus I/O's to port pin P0 ^ 0 and P0 ^ 1	
+					//route TX0 and RX0 to P0 ^ 4 and P0 ^ 5
+	 				//CP0 is enabled and the output of this comparator
+					//is routed to port pin P0 ^ 6; by cross bar priority decoder
+					//CP0 asynchoronous output pint is routed to port P0 ^ 7
+					//this is enabled to just check the difference between
+					//synchronous comparator and asynchronous comparator
+
+	/* Comparator 0 Settings */
+	CPT0CN = 0x80; //Enable Comparator0 (functional, the one above is only for CrossBar)
+
+	CPT0MX = 0x02; //Comparator0 MUX selection
+				   //Negative input is set to P1 ^ 1, and Positive input is set to P2 ^ 0
+				   // (P2 ^ 0 is the SST1, so we want to compare SST1 with the threshold voltage
+				   //of !~0.8V on P1 ^ 1
+	CPT0MD = 0x02; //Comparator0 Mode Selection
+				   //Use default, adequate TYP (CP0 Response Time, no edge triggered interrupt)
+
+    //initialize SST protocol related ports/variables/etc for ADT7486A
+	ADT7486A_init(); 	
+}
+
+
+
+/*---- User write function -----------------------------------------*/
+
+#pragma NOAREGS
+
+void user_write(unsigned char index) reentrant
+{
+   index = 0;
+   return;
+}
+
+/*---- User read function ------------------------------------------*/
+
+unsigned char user_read(unsigned char index)
+{
+   if (index);
+   return 0;
+}
+
+/*---- User function called vid CMD_USER command -------------------*/
+
+unsigned char user_func(unsigned char *data_in, unsigned char *data_out)
+{
+   /* echo input data */
+   data_out[0] = data_in[0];
+   data_out[1] = data_in[1];
+   return 2;
+}
+
+/*---- DAC functions -----------------------------------------------*/
+
+
+
+/*---- User loop function ------------------------------------------*/
+void user_loop(void)
+{
+   User_tsensor(); //run SST user-defined routines	
+}
+
+
---
+++ mscb/embedded/experiment/tsr/tsr.h
@@ -0,0 +1,159 @@
+/********************************************************************\
+
+  Name:         fgd_008.h
+  Created by:   K. Mizouchi/Pierre-André Amaudruz    Sep/20/2006
+
+
+  Contents:     Application specific (user) part of
+                Midas Slow Control Bus protocol
+                for FGD-008 SiPM HV control
+
+  $Id$
+
+\********************************************************************/
+//  need to be define FGD_008_TSR
+
+#ifndef  _TSR_H
+#define  _TSR_H
+
+/* number of HV channels */
+#define N_HV_CHN         8
+
+/* maximum current im micro Ampere */
+#define MAX_CURRENT      600
+
+/* maximum voltage in Volt */
+#define MAX_VOLTAGE      55
+
+// current measurement
+#define AMP_LT1787       8.            // amplification factor in LT1787
+#define I_SENSE_REGISTER 499.          // current sense register
+
+// HV adjust 
+#define V_ADJ_LT3010     1.275         // LT3010 ADJ  voltage 
+#define DAC_VMAX_FOR_ADJ 2*1.275       // DAC maximum voltage      
+#define DAC_VREF         2.5           // DAC reference vol = 2.5 V
+#define REGISTER_RATIO   95.3e3/2.2e6  // 2.2 M Ohm and 95.3 k Ohm registers.
+   
+/* delay for opto-couplers in us */
+#define OPT_DELAY        0    // 300
+
+/* AD7718 registers */
+#define REG_STATUS       0
+#define REG_MODE         1
+#define REG_CONTROL      2
+#define REG_FILTER       3
+#define REG_ADCDATA      4
+#define REG_ADCOFFSET    5
+#define REG_ADCGAIN      6
+#define REG_IOCONTROL    7
+
+#define ADC_SF_VALUE     82             // SF value for 50Hz rejection
+
+// chn_bits (change request flag) bit map
+#define DEMAND_CHANGED     (1<<0)
+#define RAMP_UP            (1<<1)
+#define RAMP_DOWN          (1<<2)
+
+// charge pump state for PCA control
+#define Q_PUMP_INIT          1           
+#define Q_PUMP_OFF           2           
+#define Q_PUMP_ON            3           
+
+// =======================================================================
+//      [State machine transition]
+//  
+// -------------------- Control Register Map -----------------------------
+// Bit: Name        Default Description     
+// [MSB]
+//  7 : (CONTROL_IDLE) (0) (force CPU idle)     0 : normal,  1 : force idle
+//  6 : (not assigned)  X
+//  5 : CONTROL_ISENSOR 1   I measurement       0 : disable, 1 : enable
+//  4 : CONTROL_TSENSOR 1   T measurement       0 : disable, 1 : enable
+//  3 : (not assigned)  X
+//  2 : CONTROL_IB_CTL  0   Int-bias cntrl mode 0 : disable, 1 : enable
+//  1 : CONTROL_EXT_IN  0   Ext/In bias switch  0 : INTRNL,  1 : EXTRNL   
+//  0 : CONTROL_QPUMP   0   Internal Q-Pump     0 : disable, 1 : enable
+// [LSB]
+// -----------------------------------------------------------------------
+//
+// -------------------- Status Register Map ------------------------------
+// Bit: Name                Description     
+// [MSB]
+//  7 : STATUS_RAMP_DOWN    HV Rumping down     0 : idle,    1 : processing
+//  6 : STATUS_RAMP_UP      HV Rumping up       0 : idle,    1 : processing
+//  5 : STATUS_ILIMIT       Current Limit       0 :          1 : observed
+//  4 : (not assigned))
+//  3 : STATUS_IB_CTL       INT bias control    0 : disable, 1 : enable
+//  2 : STATUS_EXT_BIAS     EXT bias            0 : disable, 1 : enable
+//  1 : STATUS_INT_BIAS     INT bias            0 : disable, 1 : enable
+//  0 : STATUS_QPUMP        Q-Pump status       0 : stopped, 1 : running
+// [LSB]
+// -----------------------------------------------------------------------
+//
+// [Abbreviations]
+//  QP  := CONTROL_QPUMP or STATUS_QPUMP (depending on the contents)
+//  E/I := CONTROL_EXT_IN
+//  ICN := CONTROL_IB_CTL or STATUS_IB_CTL 
+//  INT := STATUS_INT_BIAS
+//  EXT := STATUS_EXT_BIAS
+// -----------------------------------------------------------------------
+//
+//           STATE TABLE
+//
+//           |----------------------------------------------------|
+//           |      CONTROL        |       STATUS                 |
+//           |---------------------|------------------------------|
+//           |   ICN    E/I    QP  |    ICN    EXT    INT    QP   |
+//           |---------------------|------------------------------|
+//  STATE-1  |   X      0      0   |    0      0      0      0    |
+//  STATE-2  |   0      0      1   |    0      0      1      1    |
+//  STATE-3  |   X      1      X   |    0      1      0      0    |     
+//  STATE-4  |   1      0      1   |    1      1      1      1    |     
+//           |----------------------------------------------------|
+//   
+// =======================================================================
+
+/* CSR control bits */
+#define CONTROL_QPUMP     (1<<0)
+#define CONTROL_EXT_IN    (1<<1)
+#define CONTROL_IB_CTL    (1<<2)
+
+#define CONTROL_TSENSOR   (1<<4)
+#define CONTROL_ISENSOR   (1<<5)
+
+#define CONTROL_IDLE      (1<<7)
+
+/* CSR status bits */
+#define STATUS_QPUMP      (1<<0)
+#define STATUS_INT_BIAS   (1<<1)
+#define STATUS_EXT_BIAS   (1<<2)
+#define STATUS_IB_CTL     (1<<3)
+
+#define STATUS_ILIMIT     (1<<5)
+#define STATUS_RAMP_UP    (1<<6)
+#define STATUS_RAMP_DOWN  (1<<7)
+
+/*---- Define variable parameters returned to CMD_GET_INFO command ----*/
+
+struct user_data_type {
+   float		 internal_temp; // ADT7486A internal temperature [degree celsius]
+   float		 external_temp; // ADT7486A external temperature [degree celsius]
+   float		 external_tempOffset; // ADT7486A external temperature offset[degree celsius] 
+}; 
+
+/********************************************************************\
+
+  Application specific init and inout/output routines
+
+\********************************************************************/
+
+void user_init(unsigned char init);
+void user_loop(void);
+void user_write(unsigned char index) reentrant;
+unsigned char user_read(unsigned char index);
+unsigned char user_func(unsigned char *data_in, unsigned char *data_out);
+
+void set_current_limit(float value);
+
+#endif
