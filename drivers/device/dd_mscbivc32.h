390284be249b7c8ecf1ec2d455fdc8106e77ad7e
Pierre Amaudruz <amaudruz@triumf.ca>
2007-06-30 06:10:23 +0000

    - Initial version for the Intermediate Voltage Control for the LiXe (Triumf)
    SVN revision: 3721

---
+++ drivers/device/dd_mscbivc32.h
@@ -0,0 +1,13 @@
+/********************************************************************\
+
+  Name:         dd_mscbfgd.h
+  Created by:   Pierre-André Amaudruz
+
+  Contents:    MSCB IVC32 for the LiXe Intermediate HV control
+                Refer for documentation to:
+                http://daq-plone.triumf.ca/HR/PERIPHERALS/IVC32.pdf 
+
+  $Id:$
+\********************************************************************/
+
+ INT mscbivc32(INT cmd, ...);
