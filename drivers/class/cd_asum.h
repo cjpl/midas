2c5139ee47ce01e326c091970a8da2ae40d4d8ab
Pierre Amaudruz <amaudruz@triumf.ca>
2007-07-03 20:55:55 +0000

    - svn keys
    SVN revision: 3728

--- drivers/class/cd_asum.h
+++ drivers/class/cd_asum.h
@@ -5,7 +5,7 @@
 
   Contents:     T2K-ASUM Class Driver header file
 
-  $Id: cd_asum.h 3390 2006-10-21 06:29:55Z amaudruz $
+  $Id$
 
 \********************************************************************/
 
