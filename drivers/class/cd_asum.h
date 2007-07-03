6ed6f3812c7ed06c36257910622ec7382052ddd5
amaudruz <amaudruz@050218f5-8902-0410-8d0e-8a15d521e4f2>
2007-07-03 17:49:45 +0000

    Initial Version for V2.0 of T2K
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/midas/trunk@3723 050218f5-8902-0410-8d0e-8a15d521e4f2

---
+++ drivers/class/cd_asum.h
@@ -0,0 +1,14 @@
+/********************************************************************\
+
+  Name:         cd_asum.h
+  Created by:   Brian Lee
+
+  Contents:     T2K-ASUM Class Driver header file
+
+  $Id: cd_asum.h 3390 2006-10-21 06:29:55Z amaudruz $
+
+\********************************************************************/
+
+/* class driver routines */
+INT cd_asum(INT cmd, PEQUIPMENT pequipment);
+INT cd_asum_read(char *pevent, int);
