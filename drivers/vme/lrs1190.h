806635f40df18e929cbdf7c9f717ece00f9610b5
amaudruz <amaudruz@050218f5-8902-0410-8d0e-8a15d521e4f2>
2007-07-20 04:30:04 +0000

    correct prototypes
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/midas/trunk@3761 050218f5-8902-0410-8d0e-8a15d521e4f2

--- drivers/vme/lrs1190.h
+++ drivers/vme/lrs1190.h
@@ -26,9 +26,9 @@ void lrs1190_Reset(MVME_INTERFACE *mvme, DWORD base);
 void lrs1190_Enable(MVME_INTERFACE *mvme, DWORD base);
 void lrs1190_Disable(MVME_INTERFACE *mvme, DWORD base);
 int  lrs1190_CountRead(MVME_INTERFACE *mvme, DWORD base);
-int  LRS1190_I4DataRead(MVME_INTERFACE *mvme, DWORD base, DWORD *data);
-int  LRS1190_L2DataRead(MVME_INTERFACE *mvme, DWORD base, WORD *data);
-int  LRS1190_H2DataRead(MVME_INTERFACE *mvme, DWORD base, WORD *data);
+int  lrs1190_I4Read(MVME_INTERFACE *mvme, DWORD base, DWORD *data, int);
+int  lrs1190_L2Read(MVME_INTERFACE *mvme, DWORD base, WORD *data, int);
+int  lrs1190_H2Read(MVME_INTERFACE *mvme, DWORD base, WORD *data, int);
 
 #ifdef __cplusplus
 }
