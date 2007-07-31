ae4faf7ced0dbf78b9ce23ea182618e792519525
olchanski <olchansk@triumf.ca>
2007-07-31 21:25:10 +0000

    Converted old v895 driver to mvmestd and C++
    SVN revision: 3785

--- drivers/vme/v895.h
+++ drivers/vme/v895.h
@@ -8,47 +8,67 @@
   $Id$
 
 *********************************************************************/
-#include <stdio.h>
-#include <string.h>
-#include <stdio.h>
 
 #ifndef  __V895_INCLUDE_H__
 #define  __V895_INCLUDE_H__
 
-uint8_t v895_readReg8(const void*ptr,int ireg)
-{
-  return *(uint8_t*)(((char*)ptr) + ireg);
-}
+#include <stdio.h>
+#include <string.h>
+#include <stdio.h>
 
-void v895_writeReg8(void*ptr,int ireg,uint8_t value)
-{
-  *(uint8_t*)(((char*)ptr) + ireg) = value;
-}
+#include "mvmestd.h"
 
-uint16_t v895_readReg16(const void*ptr,int ireg)
+class v895
 {
-  return *(uint16_t*)(((char*)ptr) + ireg);
-}
+ public:
+  MVME_INTERFACE* fVme;
+  uint32_t fVmeA24;
 
-void v895_writeReg16(void*ptr,int ireg,uint16_t value)
-{
-  *(uint16_t*)(((char*)ptr) + ireg) = value;
-}
+  v895(MVME_INTERFACE* vme,  uint32_t vmeA24) // ctor
+    {
+      fVme = vme;
+      fVmeA24 = vmeA24;
+    }
 
-int v895_Status(const DWORD*ptr)
-{
-  printf("V895 at %p: fixed code (0xFAF5): 0x%x, module type: 0x%x, version: 0x%x\n",
-	 ptr,
-	 v895_readReg16(ptr,0xfa),
-	 v895_readReg16(ptr,0xfc),
-	 v895_readReg16(ptr,0xfe));
-  return 0;
-}
+  uint8_t readReg8(int ireg)
+  {
+    mvme_set_dmode(fVme, MVME_DMODE_D8);
+    return mvme_read_value(fVme, fVmeA24 + ireg);
+  }
 
-void v895_TestPulse(DWORD*ptr)
-{
-  v895_writeReg16(ptr,0x4C,1); // fire test pulse
-}
+  void writeReg8(int ireg, uint8_t value)
+  {
+    mvme_set_dmode(fVme, MVME_DMODE_D8);
+    mvme_write_value(fVme, fVmeA24 + ireg, value);
+  }
+
+  uint16_t readReg16(int ireg)
+  {
+    mvme_set_dmode(fVme, MVME_DMODE_D16);
+    return mvme_read_value(fVme, fVmeA24 + ireg);
+  }
+  
+  void writeReg16(int ireg, uint16_t value)
+  {
+    mvme_set_dmode(fVme, MVME_DMODE_D16);
+    mvme_write_value(fVme, fVmeA24 + ireg, value);
+  }
+  
+  int Status()
+  {
+    printf("V895 at VME A24 0x%x: fixed code (0xFAF5): 0x%x, module type: 0x%x, version: 0x%x\n",
+	   fVmeA24,
+	   readReg16(0xfa),
+	   readReg16(0xfc),
+	   readReg16(0xfe));
+    return 0;
+  }
+  
+  void TestPulse()
+  {
+    writeReg16(0x4C,1); // fire test pulse
+  }
+};
 
 #endif
 // end file
