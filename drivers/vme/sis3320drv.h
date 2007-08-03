d40c58fb350f5f0dc7e3ef318a6b9c4fadc0082c
olchanski <olchanski@050218f5-8902-0410-8d0e-8a15d521e4f2>
2007-08-03 04:11:18 +0000

    Partial driver for the SIS3320 8chan 12bit 200MHz ADC
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/midas/trunk@3792 050218f5-8902-0410-8d0e-8a15d521e4f2

---
+++ drivers/vme/sis3320drv.h
@@ -0,0 +1,26 @@
+/*********************************************************************
+
+  Name:         sis3320drv.h
+  Created by:   K.Olchanski
+
+  Contents:     sis3320 8-channel 200 MHz 12-bit ADC
+
+  $Id$
+                
+*********************************************************************/
+#ifndef  SIS3320DRV_INCLUDE_H
+#define  SIS3320DRV_INCLUDE_H
+
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include "mvmestd.h"
+
+#include "sis3320.h"
+
+uint32_t sis3320_RegisterRead(MVME_INTERFACE *mvme, uint32_t a32base, int offset);
+void     sis3320_RegisterWrite(MVME_INTERFACE *mvme, uint32_t a32base, int offset, uint32_t value);
+void sis3320_Reset(MVME_INTERFACE *mvme, uint32_t a32base);
+void sis3320_Status(MVME_INTERFACE *mvme, uint32_t a32base);
+
+#endif // SIS3320_INCLUDE_H
