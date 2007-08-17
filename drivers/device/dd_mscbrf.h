b26aad219fd56357d41baf80abb8887f132f1b9d
amaudruz <amaudruz@050218f5-8902-0410-8d0e-8a15d521e4f2>
2007-08-17 19:01:44 +0000

    Initial version of device driver for TITAN MPET experiment
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/midas/trunk@3806 050218f5-8902-0410-8d0e-8a15d521e4f2

---
+++ drivers/device/dd_mscbrf.h
@@ -0,0 +1,12 @@
+/********************************************************************\
+
+  Name:         dd_mscbrf.h
+  Created by:   Pierre-André Amaudruz
+
+  Contents:     Device driver function declarations for MSCB-FGD device
+
+  $Id: dd_mscbrf.h amaudruz $
+
+\********************************************************************/
+
+INT mscbrf(INT cmd, ...);
