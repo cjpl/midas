786b3ed382d5a58e685275df23fa315049e3a6bf
Pierre Amaudruz <amaudruz@triumf.ca>
2007-08-28 19:54:37 +0000

    cleaned up and got rid of codes that are only for specific applications (generalized)
    SVN revision: 3834

--- mscb/embedded/experiment/tsr/ADT7486A_tsensor.h
+++ mscb/embedded/experiment/tsr/ADT7486A_tsensor.h
@@ -3,7 +3,9 @@
 //      - Writtern for Temperature Sensor Array board
 //      - Brian Lee   (May/11/2007)
 //
-//  $id$
+//  Version:    Rev 1.2
+//
+//  $Id$
 // --------------------------------------------------------
 #ifndef  _ADT7486A_TSENSOR_H
 #define  _ADT7486A_TSENSOR_H
@@ -13,30 +15,29 @@
 // --------------------------------------------------------
 
 // Command Map (ADT7486A) - including WL and RL
-// Format is as follows:  
+// Format is as follows:
 // #define CommandName writeLength, readLength, command, data(if applicable, defined by user), channel number
-													   //dummy bytes are written for those
-													   //not applicable to fit functin prototype
-#define Ping			0x00, 0x00, 0x00, 0x00, 0x00
-#define GetIntTemp		0x01, 0x02, 0x00, 0x00, 0x00
-#define GetExt1Temp		0x01, 0x02, 0x01, 0x00, 0x00
-#define GetExt2Temp		0x01, 0x02, 0x02, 0x00, 0x00
-#define GetAllTemps		0x01, 0x06, 0x00, 0x00, 0x00
-#define SetExt1Offset	0x03, 0x00, 0xE0
-#define GetExt1Offset	0x01, 0x02, 0xE0, 0x00, 0x00
-#define SetExt2Offset	0x03, 0x00, 0xE1
-#define GetExt2Offset	0x01, 0x02, 0xE1, 0x00, 0x00
-#define ResetDevice		0x01, 0x00, 0xF6, 0x00, 0x00
-#define GetDIB_8bytes	0x01, 0x10, 0xF7, 0x00, 0x00
-#define DONT_READ_DATA 	0xff
-#define READ_DATA 		0x00
-#define TEMP_LIMIT 		30
-#define AVG_COUNT		8
+                             //dummy bytes are written for those
+                             //not applicable to fit functin prototype
+#define Ping      0x00, 0x00, 0x00, 0x00, 0x00
+#define GetIntTemp    0x01, 0x02, 0x00, 0x00, 0x00
+#define GetExt1Temp   0x01, 0x02, 0x01, 0x00, 0x00
+#define GetExt2Temp   0x01, 0x02, 0x02, 0x00, 0x00
+#define GetAllTemps   0x01, 0x06, 0x00, 0x00, 0x00
+#define SetExt1Offset 0x03, 0x00, 0xE0
+#define GetExt1Offset 0x01, 0x02, 0xE0, 0x00, 0x00
+#define SetExt2Offset 0x03, 0x00, 0xE1
+#define GetExt2Offset 0x01, 0x02, 0xE1, 0x00, 0x00
+#define ResetDevice   0x01, 0x00, 0xF6, 0x00, 0x00
+#define GetDIB_8bytes 0x01, 0x10, 0xF7, 0x00, 0x00
+#define DONT_READ_DATA  0xff
+#define READ_DATA     0x00
+#define TEMP_LIMIT    30
+#define AVG_COUNT   8
 
-void ADT7486A_init(void);
-void ADT7486A_Cmd(unsigned char addr, unsigned char writeLength, unsigned char readLength, 
-				unsigned char command, unsigned char datMSB, unsigned char datLSB, unsigned char chNum);
-void ADT7486A_LedMng(void);
-void User_tsensor(void);
+void ADT7486A_Init(void);
+void ADT7486A_Cmd(unsigned char addr, unsigned char writeLength, unsigned char readLength,
+        unsigned char command, unsigned char datMSB, unsigned char datLSB, float *dataToBeWritten);
+float ADT7486A_Read(unsigned char writeFCS_Originator, unsigned char cmdFlag);
 
 #endif
