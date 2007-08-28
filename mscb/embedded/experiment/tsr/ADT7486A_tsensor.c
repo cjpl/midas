68e4ef37887f7f3bb97f82f41995aec08c10b022
amaudruz <amaudruz@050218f5-8902-0410-8d0e-8a15d521e4f2>
2007-08-28 19:54:37 +0000

    cleaned up and got rid of codes that are only for specific applications (generalized)
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/midas/trunk@3834 050218f5-8902-0410-8d0e-8a15d521e4f2

--- mscb/embedded/experiment/tsr/ADT7486A_tsensor.c
+++ mscb/embedded/experiment/tsr/ADT7486A_tsensor.c
@@ -5,13 +5,14 @@
 
   Contents:     Temperature sensor array (ADT7486A) handler
 
-  Version:		Rev 1.1
+  Version:		Rev 1.2
 
-  Last updated: May/31/2007
+  Last updated: Jun/08/2007
   				- Finished Commenting (Rev 1.0 Completed)
 				- Got rid of exp function, replaced with different and simpler algorithm
 					(Rev 1.1)
 				- added averaging scheme
+				- Applied more modulization (SST_ReadByte() function)
 
   $Id$
 \**********************************************************************************/
@@ -19,33 +20,12 @@
 // --------------------------------------------------------
 //  Include files
 // --------------------------------------------------------
-#include    "../../mscbemb.h"
+#include "mscbemb.h"
 #include "tsr.h"
 #include "SST_handler.h"
 #include "ADT7486A_tsensor.h"
 
-/* LED declarations */
-sbit led_1 = TSRLED_1;
-sbit led_2 = TSRLED_2;
-sbit led_3 = TSRLED_3;
-sbit led_4 = TSRLED_4;
-sbit led_5 = TSRLED_5;
-sbit led_6 = TSRLED_6;
-sbit led_7 = TSRLED_7;
-
-/* MSCB user_data structure */
-extern struct user_data_type xdata user_data[N_HV_CHN];
-
-/* ADT7486A temperature array addresses 
-   --> 2 sensors per chip, so each address is repeated twice
-   --> Order does not matter unless specified correctly */
-unsigned char addrArray[] = {0x50, 0x50, 0x4F, 0x4F, 0x4D, 0x4D, 0x4C, 0x4C};
-
-//global variables for averaging purposes
-signed char k = 0;
-float dataBuffer = 0.0;
-
-void ADT7486A_init(void)
+void ADT7486A_Init(void)
 /**********************************************************************************\
 
   Routine: init_ADT7486A
@@ -61,24 +41,6 @@ void ADT7486A_init(void)
 \**********************************************************************************/
 {	
 	SST_Init();
-
-	// Turn All Leds ON
-	led_1 = LED_ON;
-	led_2 = LED_ON;
-	led_3 = LED_ON;
-	led_4 = LED_ON;
-	led_5 = LED_ON;
-	led_6 = LED_ON;
-	led_7 = LED_ON;	
-
-	delay_ms(1000);
-	led_1 = LED_OFF;
-	led_2 = LED_OFF;
-	led_3 = LED_OFF;
-	led_4 = LED_OFF;
-	led_5 = LED_OFF;
-	led_6 = LED_OFF;
-	led_7 = LED_OFF;
 }
 
 void ADT7486A_LedMng(void)
@@ -100,18 +62,17 @@ void ADT7486A_LedMng(void)
 {
 	//for each external temperature source, if it reaches over 30 degree
 	//celsius, then turn on the according LED ON, otherwise OFF
-	led_1 = (user_data[0].external_temp >= TEMP_LIMIT) ? LED_ON : LED_OFF;
+	/*led_1 = (user_data[0].external_temp >= TEMP_LIMIT) ? LED_ON : LED_OFF;
 	led_2 = (user_data[1].external_temp >= TEMP_LIMIT) ? LED_ON : LED_OFF;
 	led_3 = (user_data[2].external_temp >= TEMP_LIMIT) ? LED_ON : LED_OFF;
 	led_4 = (user_data[3].external_temp >= TEMP_LIMIT) ? LED_ON : LED_OFF;
 	led_5 = (user_data[5].internal_temp >= TEMP_LIMIT) ? LED_ON : LED_OFF;
 	led_6 = (user_data[6].external_temp >= TEMP_LIMIT) ? LED_ON : LED_OFF;
-	led_7 = (user_data[7].external_temp >= TEMP_LIMIT) ? LED_ON : LED_OFF;
+	led_7 = (user_data[7].external_temp >= TEMP_LIMIT) ? LED_ON : LED_OFF;*/
 }
-			
 
 void ADT7486A_Cmd(unsigned char addr, unsigned char writeLength, unsigned char readLength, 
-				unsigned char command, unsigned char datMSB, unsigned char datLSB, unsigned char chNum)
+				unsigned char command, unsigned char datMSB, unsigned char datLSB, float *varToBeWritten)
 /**********************************************************************************\
 
   Routine: ADT7486A_Cmd 
@@ -137,6 +98,8 @@ void ADT7486A_Cmd(unsigned char addr, unsigned char writeLength, unsigned char r
 \**********************************************************************************/
 {
 	float tempDataBuffer = 0x00;
+	signed char k = 0;
+	float dataBuffer = 0.0;
 	unsigned char writeFCS_Org = 0x00; //originator's side write FCS	
 
 	//Calculate originator's side write FCS
@@ -148,121 +111,154 @@ void ADT7486A_Cmd(unsigned char addr, unsigned char writeLength, unsigned char r
 		writeFCS_Org = FCS_Step(datMSB, FCS_Step(datLSB, writeFCS_Org));
 	}
 
-	//making sure the SST pin is set to push-pull before originating msgs
-	P2MDOUT = 0x13; 
+		//Run averaging starting here
+	while(k != AVG_COUNT)
+	{
+		//making sure the SST pin is set to push-pull before originating msgs
+		P2MDOUT = 0x01; 
 
-	//Start the message
-	SST_DrvLow();
-	SST_DrvLow();
-	SST_DrvByte(addr); //target address
-	SST_DrvLow();
-	SST_DrvByte(writeLength); //WriteLength
-	SST_DrvByte(readLength); //ReadLength
-	if(writeLength != 0x00)
-	{
-		SST_DrvByte(command); //Optional : Commands
-	}
-	else //writeLength == 0x00, Ping command
-	{
-		SST_Read(writeFCS_Org, DONT_READ_DATA);
-	}
-	if(writeLength == 0x03) //If there is data to be sent (0x03 writeLength)
-	{
-		//Send the data in little endian format, LSB before MSB
-		SST_DrvByte(datLSB);
-		SST_DrvByte(datMSB);
-		SST_Read(writeFCS_Org, DONT_READ_DATA);
-	}
-
-	//In this application only temperature measurements are really required,
-	//so we just need 2 bytes of reading
-	if((writeLength == 0x01) && (readLength == 0x02)) 
-	{
-		if(k != AVG_COUNT)
+		//Start the message
+		SST_DrvLow();
+		SST_DrvLow();
+		SST_DrvByte(addr); //target address
+		SST_DrvLow();
+		SST_DrvByte(writeLength); //WriteLength
+		SST_DrvByte(readLength); //ReadLength
+		if(writeLength != 0x00)
 		{
-			tempDataBuffer = SST_Read(writeFCS_Org, READ_DATA); //Get internal temperature	
-			if((tempDataBuffer != -500.0) && (tempDataBuffer != -300.0) && (tempDataBuffer != 0))
-			{
-				dataBuffer += tempDataBuffer;
-				k++;
-			}
+			SST_DrvByte(command); //Optional : Commands
 		}
-		if(k == AVG_COUNT)
-		{		
-			if(command == 0x00) //Get internal temperature
-			{
-				DISABLE_INTERRUPTS;			
-				user_data[chNum].internal_temp = dataBuffer / k;
-				ENABLE_INTERRUPTS;				
-			}
-			else if((command == 0x01) || (command == 0x02)) //Get external temperature
-			{
-				DISABLE_INTERRUPTS;
-				user_data[chNum].external_temp = dataBuffer / k;
-				ENABLE_INTERRUPTS;
-			}
-			else if((command == 0xe0) || (command == 0xe1)) //Get external temperature offset
-			{
-				DISABLE_INTERRUPTS;
-				user_data[chNum].external_tempOffset = dataBuffer / k;
-				ENABLE_INTERRUPTS;
-			}
-			//reset averaging buffer variable
-			dataBuffer = 0.0;
-		}		
-	}
-	else if(command == 0xF6)//if reset command
-	{
-		SST_Read(writeFCS_Org, DONT_READ_DATA);
-	}
+		else //writeLength == 0x00, Ping command
+		{			
+			ADT7486A_Read(writeFCS_Org, DONT_READ_DATA);
+			break;
+		}
+		if(writeLength == 0x03) //If there is data to be sent (0x03 writeLength)
+		{
+			//Send the data in little endian format, LSB before MSB
+			SST_DrvByte(datLSB);
+			SST_DrvByte(datMSB);
+			ADT7486A_Read(writeFCS_Org, DONT_READ_DATA);
+			break;
+		}
 
-	//Delay for the next msg
-	SST_Clear();
+		//In this application only temperature measurements are really required,
+		//so we just need 2 bytes of reading
+		if((writeLength == 0x01) && (readLength == 0x02)) 
+		{
+			if(k != AVG_COUNT)
+			{
+				tempDataBuffer = ADT7486A_Read(writeFCS_Org, READ_DATA); //Get internal temperature	
+				if((tempDataBuffer != -500.0) && (tempDataBuffer != -300.0) && (tempDataBuffer != 0))
+				{
+					dataBuffer += tempDataBuffer;
+					k++;
+				}
+			}
+			if(k == AVG_COUNT)
+			{		
+				DISABLE_INTERRUPTS;
+				if(dataBuffer != (508.0 * AVG_COUNT))
+				{
+					*varToBeWritten = dataBuffer / k;
+				}
+				ENABLE_INTERRUPTS;
+
+				//reset averaging buffer variable
+				dataBuffer = 0.0;
+				
+				//reset the counting variable used for averaging and break the loop
+				k = 0;
+				break;
+			}		
+		}
+		else if(command == 0xF6)//if reset command
+		{
+			ADT7486A_Read(writeFCS_Org, DONT_READ_DATA);
+		}
+
+		//Delay for the next msg
+		SST_Clear();
+	}
 }
 
-void User_tsensor(void)
+float ADT7486A_Read(unsigned char writeFCS_Originator, unsigned char cmdFlag)
 /**********************************************************************************\
 
-  Routine: User_tsensor
+  Routine: SST_Read
 
-  Purpose: User routine for ADT7486A temperature sensors with SST protocol
+  Purpose: Read only needs to be working for reading 2 bytes of info
+		   Which are Internal Temp, External Temp1, and External Temp2
+		   Others should be verified on oscilloscope since mscb returns a data at maximum
+		   4bytes, (allTemp and GetDIB commands take 6 bytes and 16 bytes, which is impossible anyway)
 
   Input:
-    void
+    unsigned char writeFCS_Originator	Originator's writeFCS 
+	unsigned char cmdFlag				a Flag used to distinguish
+										if the command is a Ping/Reset
+										or not
 
   Function value:
-    void
+    float								value of Data given by 
+										the Client in response to
+										Originator's commands
+										(converted Temperature value)
 
 \**********************************************************************************/
 {	
-	//Run a SST command
-	//read the current channel's temperature
-	/*
-	if((cur_sub_addr() % 2) == 0) //even channel numbers
-	{
-		ADT7486A_Cmd(addrArray[cur_sub_addr()], GetExt1Temp, cur_sub_addr()); //Get external temperature 1 command
-							   			 //don't forget to specify the correct address of ADT7486A chip
-		ADT7486A_Cmd(addrArray[cur_sub_addr()], GetExt1Offset, cur_sub_addr());
-		ADT7486A_Cmd(addrArray[cur_sub_addr()], GetIntTemp, cur_sub_addr());
-		//ADT7486A_Cmd(addrArray[n], SetExt1Offset, 0x05, 0x05, n);
-		//ADT7486A_Cmd(addrArray[n], ResetDevice, n);
-	}
-	else if((cur_sub_addr() % 2) != 0) //odd channel numbers
-	{
-		ADT7486A_Cmd(addrArray[cur_sub_addr()], GetExt2Temp, cur_sub_addr()); //Get external temperature 1 command
-							   			 //don't forget to specify the correct address of ADT7486A chip
-		ADT7486A_Cmd(addrArray[cur_sub_addr()], GetExt2Offset, cur_sub_addr());
-		ADT7486A_Cmd(addrArray[cur_sub_addr()], GetIntTemp, cur_sub_addr());
-	}
-	ADT7486A_LedMng();
-	*/
+	//declare local variables
+	unsigned char LSB_Received = 0x00; //Max 2bytes of info from 3 different possible commands
+	unsigned char MSB_Received = 0x00; //for reading, GetIntTemp, GetExtTemp1, GetExtTemp2
+	unsigned char writeFCS_Client = 0x00;
+	unsigned char readFCS_Client = 0x00; //used for both writeFCS and readFCS
+	float convertedTemp = 0.0;
+
+	writeFCS_Client = SST_ReadByte(); //get Write FFCS from Client
 	
-	SST_SwitchLine(1); //Switch to SST1 line
-	while(k != 8) ADT7486A_Cmd(addrArray[4], GetIntTemp, 5); //Get internal temperature 1 command		
-	k = 0; //reset counter
-	while(k != 8) ADT7486A_Cmd(addrArray[4], GetExt1Temp, 6); //Get external temperature 1 command
-	k = 0; //reset counter
-	while(k != 8) ADT7486A_Cmd(addrArray[5], GetExt2Temp, 7); //Get external temperature 1 command
-	k = 0; //reset counter
-	ADT7486A_LedMng();
+	//writeFCS check
+	if(writeFCS_Originator != writeFCS_Client)
+	{
+		return ((float) -300.0); //if the FCS's don't match up, then return -300
+	}
+
+	if(cmdFlag == 0x00) //If it's not the Ping Command or SetOffSet command
+	{
+		//Take the data in to a variable called datareceived
+		LSB_Received = SST_ReadByte(); //get LSB
+		MSB_Received = SST_ReadByte(); //get MSB
+		readFCS_Client = SST_ReadByte(); //get Read FCS from Client
+
+		//Here, reading data is finished, now analyze the received data
+
+		//Check FCS
+		//returns -300 because absolute low temperature is -273 degree Celsius, so this value
+		//suitable for error reporting value
+		//test = FCS(cmdData, 6); //then check for 5 bytes	
+		if(FCS_Step(MSB_Received, FCS_Step(LSB_Received, 0x00)) != readFCS_Client) //readFCS check
+		{
+			return ((float) -300.0); //if the FCS's don't match up, then return -300
+		}		
+	
+		//Conver the Temperature according to the structure of bits defined by
+		//ADT7486A manual
+		//Convert 2bytes of temperature from 
+		//GetExt1OffSet/GetExt2OffSet/GetIntTemp()/GetExt1Temp()/GetExt2Temp() commands
+		//Mask out the first bit (sign bit)
+		//and if the sign bit is 1, then do two's complement
+		if((MSB_Received & 0x80) == 0x80) 
+		{
+			MSB_Received = ~MSB_Received;
+			LSB_Received = ~LSB_Received + 0x01;
+		}
+		
+		//Calculate the converted Temperature
+		convertedTemp = ((float) (MSB_Received * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 0.015625)) + ((float) (LSB_Received * 0.015625));
+	}
+	else //If it is the Ping Command or SetOffSet command
+	{
+		return -500;
+	}
+
+	//return the converted temperature value
+	return convertedTemp;
 }
