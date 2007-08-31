3b548b7bf2cab23e3ecd2084ec856cc9f67f31fc
amaudruz <amaudruz@050218f5-8902-0410-8d0e-8a15d521e4f2>
2007-08-31 23:30:03 +0000

    See comments about ADT7486A addressing, If the address is wrong, then the whole t2k-asum operations will not run
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/midas/trunk@3863 050218f5-8902-0410-8d0e-8a15d521e4f2

--- mscb/embedded/experiment/t2k/ADT7486A_tsensor.h
+++ mscb/embedded/experiment/t2k/ADT7486A_tsensor.h
@@ -3,7 +3,7 @@
   Created by:   Brian Lee						     May/11/2007
 
 
-  Contents:     Temperature sensor array (ADT7486A) handler
+  Contents:     Temperature sensor array (ADT7486A) handler for t2k-asum board
 
   Version:		Rev 1.2
 
@@ -36,8 +36,14 @@
 #define DONT_READ_DATA  0xff
 #define READ_DATA     0x00
 #define TEMP_LIMIT    30
-#define AVG_COUNT   1
-#define ADT7486A_CONVERSION_TIME 38 //in ms
+#define AVG_COUNT   8 //the time communication takes is, 3.5ms times this number
+#define ADT7486A_CONVERSION_TIME 0 //in ms, it has buffers, don't really need to wait
+/* ADT7486A temperature array addresses 
+   --> 2 sensors per chip, so each address is repeated twice
+   --> Order does not matter unless specified correctly */
+#define ADT7486A_ADDR_ARRAY 0x4C //0x4B for the first board, 0x4C for the 2nd Board with green tag on
+// also define number of temperature sensors you have on board (final design would be 4 ADT7486A's, 8 Remote)
+#define ADT7486A_NUM 1 //currently, for the prototype, there is only one
 
 void ADT7486A_Init(void);
 void ADT7486A_Cmd(unsigned char addr, unsigned char writeLength, unsigned char readLength,
