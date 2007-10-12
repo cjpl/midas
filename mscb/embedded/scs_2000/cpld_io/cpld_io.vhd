164ee33c0d8c3e3853dda0cd17b05e086b1d0b48
ritt@PSI.CH <ritt@PSI.CH@050218f5-8902-0410-8d0e-8a15d521e4f2>
2007-10-12 08:40:13 +0000

    Initial revision
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/midas/trunk@3981 050218f5-8902-0410-8d0e-8a15d521e4f2

---
+++ mscb/embedded/scs_2000/cpld_io/cpld_io.vhd
@@ -0,0 +1,37 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity cpld_io is
+    Port ( P_I_CS1         : in std_logic;
+           P_I_SDI         : in std_logic;
+           P_I_SCLK        : in std_logic;
+           P_IO_SDO        : inout std_logic;
+           P_IO_INTERN     : inout std_logic_vector(7 downto 0);
+           P_O_1A          : out std_logic_vector(7 downto 0);
+           P_I_2A          : in std_logic_vector(7 downto 0);
+           P_O_CLKSEL      : out std_logic_vector(3 downto 0);
+           P_I_CLK         : in std_logic;
+           P_O_DUMMY       : out std_logic
+         );
+end cpld_io;
+
+architecture Behavioral of cpld_io is
+
+begin
+
+  -- quarz speed in MHz:
+  -- 0:100, 1:33.33, 2:30, 3:120, 4:25, 5:20, 6:70, 7:80
+  -- 8:75, 9:66.66, 10:60, 11:60, 12: 50, 13:45, 14:90, 15:40 
+  P_O_CLKSEL  <= "0000";
+  
+  -- serial bus
+  P_IO_SDO <= 'Z';
+  P_O_DUMMY <= P_IO_SDO xor P_I_CS1 xor P_I_SDI xor P_I_SCLK;
+  
+  -- simply send input to central CPLD
+  P_IO_INTERN <= P_I_2A;
+  P_O_1A <= "00000000"; 
+  
+end Behavioral;
