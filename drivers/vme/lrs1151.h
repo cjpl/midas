1eb8c696edf1b995ebec4246d7b635506483c545
amaudruz <amaudruz@050218f5-8902-0410-8d0e-8a15d521e4f2>
2007-11-01 17:47:12 +0000

    - converted driver from VxWorks
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/midas/trunk@4019 050218f5-8902-0410-8d0e-8a15d521e4f2

---
+++ drivers/vme/lrs1151.h
@@ -0,0 +1,41 @@
+/*********************************************************************
+  Name:         lrs1151.h
+  Created by:   Pierre-Andre Amaudruz
+
+  Contents:     lrs1151 LeCroy 16channels scaler
+
+  $Id:$
+*********************************************************************/
+#ifndef  LRS1151_INCLUDE_H
+#define  LRS1151_INCLUDE_H
+
+#include <stdio.h>
+#include <string.h>
+#include "mvmestd.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define  LRS1151_CLEAR_ALL       (0x0)
+#define  LRS1151_CLEAR_WO        (DWORD) (0x0040)
+#define  LRS1151_DATA_RO         (DWORD) (0x0080)
+
+
+void lrs1151_Reset(MVME_INTERFACE *mvme, DWORD base, int ch);
+int  lrs1151_Read(MVME_INTERFACE *mvme, DWORD base, DWORD *data);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // _INCLUDE_H
+
+/* emacs
+ * Local Variables:
+ * mode:C
+ * mode:font-lock
+ * tab-width: 8
+ * c-basic-offset: 2
+ * End:
+ */
