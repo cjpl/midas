e80d1ef6a7ddabe51dbc767c25c9979758ff9e4b
amaudruz <amaudruz@050218f5-8902-0410-8d0e-8a15d521e4f2>
2009-01-18 19:49:38 +0000

    include Setup function
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/midas/trunk@4438 050218f5-8902-0410-8d0e-8a15d521e4f2

--- drivers/vme/sis3320drv.h
+++ drivers/vme/sis3320drv.h
@@ -22,5 +22,6 @@ uint32_t sis3320_RegisterRead(MVME_INTERFACE *mvme, uint32_t a32base, int offset
 void     sis3320_RegisterWrite(MVME_INTERFACE *mvme, uint32_t a32base, int offset, uint32_t value);
 void sis3320_Reset(MVME_INTERFACE *mvme, uint32_t a32base);
 void sis3320_Status(MVME_INTERFACE *mvme, uint32_t a32base);
+int  sis3320_Setup(MVME_INTERFACE *mvme, uint32_t a32base, int mode);
 
 #endif // SIS3320_INCLUDE_H
