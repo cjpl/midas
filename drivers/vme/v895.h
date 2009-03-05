ae277b0f55cd53cf59391486d1f85c5e8f6f2c9f
olchanski <olchanski@050218f5-8902-0410-8d0e-8a15d521e4f2>
2009-03-05 00:26:34 +0000

    Replace C++ class based driver with a normal C-callable driver
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/midas/trunk@4457 050218f5-8902-0410-8d0e-8a15d521e4f2

--- drivers/vme/v895.h
+++ drivers/vme/v895.h
@@ -18,57 +18,48 @@
 
 #include "mvmestd.h"
 
-class v895
+uint8_t v895_readReg8(MVME_INTERFACE *mvme,int base,int ireg)
 {
- public:
-  MVME_INTERFACE* fVme;
-  uint32_t fVmeA24;
+  mvme_set_am(mvme,MVME_AM_A24);
+  mvme_set_dmode(mvme,MVME_DMODE_D8);
+  return mvme_read_value(mvme,base + ireg);
+}
 
-  v895(MVME_INTERFACE* vme,  uint32_t vmeA24) // ctor
-    {
-      fVme = vme;
-      fVmeA24 = vmeA24;
-    }
+void v895_writeReg8(MVME_INTERFACE *mvme,int base,int ireg,uint8_t value)
+{
+  mvme_set_am(mvme,MVME_AM_A24);
+  mvme_set_dmode(mvme,MVME_DMODE_D8);
+  mvme_write_value(mvme,base + ireg,value);
+}
 
-  uint8_t readReg8(int ireg)
-  {
-    mvme_set_dmode(fVme, MVME_DMODE_D8);
-    return mvme_read_value(fVme, fVmeA24 + ireg);
-  }
+uint16_t v895_readReg16(MVME_INTERFACE *mvme,int base,int ireg)
+{
+  mvme_set_am(mvme,MVME_AM_A24);
+  mvme_set_dmode(mvme,MVME_DMODE_D16);
+  return mvme_read_value(mvme,base + ireg);
+}
 
-  void writeReg8(int ireg, uint8_t value)
-  {
-    mvme_set_dmode(fVme, MVME_DMODE_D8);
-    mvme_write_value(fVme, fVmeA24 + ireg, value);
-  }
+void v895_writeReg16(MVME_INTERFACE *mvme,int base,int ireg,uint16_t value)
+{
+  mvme_set_am(mvme,MVME_AM_A24);
+  mvme_set_dmode(mvme,MVME_DMODE_D16);
+  mvme_write_value(mvme,base + ireg,value);
+}
 
-  uint16_t readReg16(int ireg)
-  {
-    mvme_set_dmode(fVme, MVME_DMODE_D16);
-    return mvme_read_value(fVme, fVmeA24 + ireg);
-  }
-  
-  void writeReg16(int ireg, uint16_t value)
-  {
-    mvme_set_dmode(fVme, MVME_DMODE_D16);
-    mvme_write_value(fVme, fVmeA24 + ireg, value);
-  }
-  
-  int Status()
-  {
-    printf("V895 at VME A24 0x%x: fixed code (0xFAF5): 0x%x, module type: 0x%x, version: 0x%x\n",
-	   fVmeA24,
-	   readReg16(0xfa),
-	   readReg16(0xfc),
-	   readReg16(0xfe));
-    return 0;
-  }
-  
-  void TestPulse()
-  {
-    writeReg16(0x4C,1); // fire test pulse
-  }
-};
+int v895_Status(MVME_INTERFACE *mvme,int base)
+{
+  printf("V895 at VME A24 0x%x: fixed code (0xFAF5): 0x%x, module type: 0x%x, version: 0x%x\n",
+	 base,
+	 v895_readReg16(mvme,base,0xfa),
+	 v895_readReg16(mvme,base,0xfc),
+	 v895_readReg16(mvme,base,0xfe));
+  return 0;
+}
+
+void v895_TestPulse(MVME_INTERFACE *mvme,int base)
+{
+  v895_writeReg16(mvme,base,0x4C,1); // fire test pulse
+}
 
 #endif
 // end file
