fd74b02ee43678244eaeae007a91825cff0af461
Pierre Amaudruz <amaudruz@triumf.ca>
2010-03-17 20:19:42 +0000

    include std scaler read
    SVN revision: 4692

--- drivers/vme/sis3820drv.h
+++ drivers/vme/sis3820drv.h
@@ -14,6 +14,7 @@
 #include <stdint.h>
 #include <string.h>
 #include "mvmestd.h"
+#include "sis3820.h"
 
 #ifndef  SIS3820DRV_INCLUDE_H
 #define  SIS3820DRV_INCLUDE_H
@@ -22,6 +23,7 @@ uint32_t sis3820_RegisterRead(MVME_INTERFACE *mvme, DWORD base, int offset);
 void     sis3820_RegisterWrite(MVME_INTERFACE *mvme, DWORD base, int offset, uint32_t value);
 int  sis3820_FifoRead(MVME_INTERFACE *mvme, DWORD base, void *pdest, int wcount);
 int  sis3820_DataReady(MVME_INTERFACE *mvme, DWORD base);
+uint32_t sis3820_ScalerRead(MVME_INTERFACE *mvme, DWORD base, int ch);
 void sis3820_Reset(MVME_INTERFACE *mvme, DWORD base);
 void sis3820_Status(MVME_INTERFACE *mvme, DWORD base);
 
