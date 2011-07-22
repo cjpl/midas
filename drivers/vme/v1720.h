418a009ef735d403a393333df1053a514e36b46b
Stefan Ritt <stefan.ritt@psi.ch>
2011-07-22 16:17:40 +0000

    add ZLE regs
    SVN revision: 5166

--- drivers/vme/v1720.h
+++ drivers/vme/v1720.h
@@ -31,9 +31,9 @@
 #define V1720_ROC_FPGA_FW_REV                 0x8124      /* read  only; D32 */ 
 #define V1720_EVENT_STORED                    0x812C      /* read  only; D32 */ 
 #define V1720_SET_MONITOR_DAC                 0x8138      /* R/W       ; D32 */ 
-#define V1720_BOARD_INFO                      0x8140	    /* read  only; D32 */ 
+#define V1720_BOARD_INFO                      0x8140	  /* read  only; D32 */ 
 #define V1720_MONITOR_MODE                    0x8144      /* R/W       ; D32 */ 
-#define V1720_EVENT_SIZE                      0x814C	    /* read  only; D32 */ 
+#define V1720_EVENT_SIZE                      0x814C	  /* read  only; D32 */ 
 #define V1720_VME_CONTROL                     0xEF00      /* R/W       ; D32 */ 
 #define V1720_VME_STATUS                      0xEF04      /* read  only; D32 */ 
 #define V1720_BOARD_ID                        0xEF08      /* R/W       ; D32 */ 
@@ -50,6 +50,8 @@
 #define V1720_CONFIG_RELOAD                   0xEF34      /* write only; D32 */ 
 #define V1720_CONFIG_ROM                      0xF000      /* read  only; D32 */ 
 
+#define V1720_ZS_THRESHOLD                    0x1024      /* For channel 0 */
+#define V1720_ZS_NSAMP                        0x1028      /* For channel 0 */
 #define V1720_CHANNEL_THRESHOLD               0x1080      /* For channel 0 */
 #define V1720_CHANNEL_OUTHRESHOLD             0x1084      /* For channel 0 */
 #define V1720_CHANNEL_STATUS                  0x1088      /* For channel 0 */
@@ -64,6 +66,13 @@
 #define V1720_MULTI_BOARD_SYNC_MODE                 0x0006
 #define V1720_COUNT_ACCEPTED_TRIGGER                0x0007
 #define V1720_COUNT_ALL_TRIGGER                     0x0008
+#define V1720_TRIGGER_OVERTH                        0x0001
+#define V1720_TRIGGER_UNDERTH                       0x0002
+#define V1720_PACK25_ENABLE                         0x0003
+#define V1720_PACK25_DISABLE                        0x0004
+#define V1720_NO_ZERO_SUPPRESSION                   0x0005
+#define V1720_ZLE                                   0x0006
+#define V1720_ZS_AMP                                0x0007
 
 #define V1720_EVENT_CONFIG_ALL_ADC        0x01000000
 #define V1720_SOFT_TRIGGER                0x80000000
