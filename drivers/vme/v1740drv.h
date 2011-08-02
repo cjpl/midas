de254aec936b1c5152ed527d8362ff4ac8c67cde
Stefan Ritt <stefan.ritt@psi.ch>
2011-08-02 19:49:06 +0000

    add prototypes for group
    SVN revision: 5172

--- drivers/vme/v1740drv.h
+++ drivers/vme/v1740drv.h
@@ -1,9 +1,9 @@
 /*********************************************************************
 
   Name:         v1740drv.h
-  Created by:   P.-A. Amaudruz
+  Created by:   K.Olchanski
 
-  Contents:     v1740 64-channels 65Msps 12-bit ADC
+  Contents:     v1740 8-channel 200 MHz 12-bit ADC
 
   $Id$
                 
@@ -29,12 +29,19 @@ void     v1740_TrgCtl(MVME_INTERFACE *mvme, uint32_t a32base, uint32_t reg, uint
 
 void     v1740_RegisterWrite(MVME_INTERFACE *mvme, uint32_t a32base, int offset, uint32_t value);
 void     v1740_Reset(MVME_INTERFACE *mvme, uint32_t a32base);
-void     v1740_GroupThreshold(MVME_INTERFACE *mvme, uint32_t base, uint16_t group, uint16_t threshold);
-void     v1740_GroupDAC(MVME_INTERFACE *mvme, uint32_t base, uint16_t group, uint16_t dac);
+
 void     v1740_Status(MVME_INTERFACE *mvme, uint32_t a32base);
 int      v1740_Setup(MVME_INTERFACE *mvme, uint32_t a32base, int mode);
 void     v1740_info(MVME_INTERFACE *mvme, uint32_t a32base, int *nch, uint32_t *n32w);
 uint32_t v1740_DataRead(MVME_INTERFACE *mvme,uint32_t a32base, uint32_t *pdata, uint32_t n32w);
 uint32_t v1740_DataBlockRead(MVME_INTERFACE *mvme, uint32_t a32base, uint32_t *pdest, uint32_t *nentry);
+void     v1740_GroupThresholdSet(MVME_INTERFACE *mvme, uint32_t base, uint32_t channel, uint32_t threshold);
+void     v1740_GroupOUThresholdSet(MVME_INTERFACE *mvme, uint32_t base, uint32_t channel, uint32_t threshold);
+void     v1740_GroupDACSet(MVME_INTERFACE *mvme, uint32_t base, uint32_t channel, uint32_t dac);
+int      v1740_GroupDACGet(MVME_INTERFACE *mvme, uint32_t base, uint32_t channel, uint32_t *dac);
+void     v1740_GroupSet(MVME_INTERFACE *mvme, uint32_t base, uint32_t channel, uint32_t what, uint32_t that);
+uint32_t v1740_GroupGet(MVME_INTERFACE *mvme, uint32_t base, uint32_t channel, uint32_t what);
+void     v1740_GroupConfig(MVME_INTERFACE *mvme, uint32_t base, uint32_t operation);
+void     v1740_Align64Set(MVME_INTERFACE *mvme, uint32_t base);
 
 #endif // v1740DRV_INCLUDE_H
