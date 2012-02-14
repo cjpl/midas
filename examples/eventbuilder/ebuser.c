1403cc0d7499a0459c071f43a3922ff9d5f74aef
Pierre Amaudruz <amaudruz@triumf.ca>
2012-02-14 19:55:00 +0000

    update comment including bk_copy
    SVN revision: 5257

--- examples/eventbuilder/ebuser.c
+++ examples/eventbuilder/ebuser.c
@@ -13,10 +13,12 @@ The Event builder user file
 */
 
 #include <stdio.h>
+#include <string.h>
 #include "midas.h"
 #include "mevb.h"
-//#include "ybos.h"
 
+/* data type sizes */
+extern INT tid_size[];
 /*-- Globals -------------------------------------------------------*/
 
 /* The frontend name (client name) as seen by other MIDAS clients   */
@@ -73,9 +75,8 @@ EQUIPMENT equipment[] = {
 #ifdef __cplusplus
 }
 #endif
-/********************************************************************/
-/********************************************************************/
 
+/********************************************************************/
 /********************************************************************/
 INT ebuilder_init()
 {
@@ -132,7 +133,20 @@ all fragments of the same serial number. The destination
 event has already the final EVENT_HEADER setup with
 the data size set to 0. It is than possible to
 add private data at this point using the proper
-bank calls.
+bank calls. Therefore any new banks created within eb_user will be appearing
+before the collected banks from the fragments.
+When using the eb_user with the ODB flag "user build=y" 
+(equipments/EB/settings) the automatic event builder is skipped.
+This allow the user to extract selectively from the different fragments the
+appropriate banks and compose the final destination event. In order to
+do so, the function "bk_copy(pevent, ebch[i].pfragment, bankname)" will copy
+a particular bank from a given fragment.
+
+<b>Note:</b> It exists two Midas event format to address bank size less than 32KB and 
+larger bank size <4GB. This distinction is done by the call bk_init(pevent) for the small
+bank size and bk_init32(pevent) for large bank size. Within an experiment, this
+declaration has to be consistant. Therefore the bk_init in the eb_user should follow
+as well the type of the frontends.
 
 The ebch[] array structure points to nfragment channel structure
 with the following content:
@@ -147,37 +161,28 @@ typedef struct {
 
 The correct code for including your own MIDAS bank is shown below where
 \b TID_xxx is one of the valid Bank type starting with \b TID_ for
-midas format or \b xxx_BKTYPE for Ybos data format.
-\b bank_name is a 4 character descriptor.
+midas format \b bank_name is a 4 character descriptor.
 \b pdata has to be declared accordingly with the bank type.
 Refers to the ebuser.c source code for further description.
 
 <strong>
 It is not possible to mix within the same destination event different event format!
+No bk_swap performed when user build is requested.
 </strong>
 
 \code
   // Event is empty, fill it with BANK_HEADER
   // If you need to add your own bank at this stage
 
-  bk_init(pevent);
+  // Need to match the decalration in the Frontends.
+  bk_init(pevent);  
+//  bk_init32(pevent);
   bk_create(pevent, bank_name, TID_xxxx, &pdata);
   *pdata++ = ...;
   *dest_size = bk_close(pevent, pdata);
   pheader->data_size = *dest_size + sizeof(EVENT_HEADER);
 \endcode
 
-For YBOS format, use the following example.
-
-\code
-  ybk_init(pevent);
-  ybk_create(pevent, "EBBK", I4_BKTYPE, &pdata);
-  *pdata++ = 0x12345678;
-  *pdata++ = 0x87654321;
-  *dest_size = ybk_close(pevent, pdata);
-  *dest_size *= 4;
-  pheader->data_size = *dest_size + sizeof(YBOS_BANK_HEADER);
-\endcode
 @param nfrag Number of fragment.
 @param mismatch Midas Serial number mismatch flag.
 @param ebch  Structure to all the fragments.
@@ -189,12 +194,10 @@ For YBOS format, use the following example.
 INT eb_user(INT nfrag, BOOL mismatch, EBUILDER_CHANNEL * ebch
             , EVENT_HEADER * pheader, void *pevent, INT * dest_size)
 {
-  INT i, frag_size, serial;
-  DWORD *psrcData;
-  DWORD  *pdata;
-  INT my_trigger_mask;
+  INT i, frag_size, serial, status;
+  DWORD  *pdata, *psrcData;
 
-  //if(debug)printf("Entering eb_user\n");
+
   //
   // Do some extra fragment consistency check
   if (mismatch){
@@ -209,22 +212,20 @@ INT eb_user(INT nfrag, BOOL mismatch, EBUILDER_CHANNEL * ebch
 
   //
   // Include my own bank
-  //  bk_init(pevent);
-  //bk_create(pevent, "MYOW", TID_DWORD, &pdata);
-  //for (i = 0; i < nfrag; i++) {
-  //  *pdata++ = ((EVENT_HEADER *) ebch[i].pfragment)->serial_number;
-  //  *pdata++ = ((EVENT_HEADER *) ebch[i].pfragment)->time_stamp;
-  // }
-  //*dest_size = bk_close(pevent, pdata);
-  //pheader->data_size = *dest_size + sizeof(EVENT_HEADER);
-
-  //Extract the trigger_mask from the VMIC fragment and put it
-  //to the EB event
-  if(0) {
-    my_trigger_mask = ((EVENT_HEADER *) ebch[0].pfragment)->trigger_mask;
-    pheader-> trigger_mask = my_trigger_mask;
+  bk_init(pevent);
+  bk_create(pevent, "MYOW", TID_DWORD, &pdata);
+  for (i = 0; i < nfrag; i++) {
+    *pdata++ = ((EVENT_HEADER *) ebch[i].pfragment)->serial_number;
+    *pdata++ = ((EVENT_HEADER *) ebch[i].pfragment)->time_stamp;
   }
-  //printf("Trig mask from VMIC 0x%x\n",my_trigger_mask);
+  *dest_size = bk_close(pevent, pdata);
+  pheader->data_size = *dest_size + sizeof(EVENT_HEADER);
+
+  // Copy the bank TC01 if found from fragment0 to the destination
+  status = bk_copy(pevent, ebch[0].pfragment, "TC01");
+  if (status == EB_BANK_NOT_FOUND) {
+    printf("bank TC01 not found\n");
+  }
 
   // Destination access
   // dest_serial = pheader->serial_number;
