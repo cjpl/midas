5cb0c9e5f1b335b97d3470d80bf68e7a6bd44959
Konstantin Olchanski <olchansk@ladd00.triumf.ca>
2013-08-13 16:44:27 -0700

    Constify more hs_xxx() functions. Note: TAGS passed to hs_define_event() have to be fully memset to zero before use (or use calloc() instead of malloc()).

--- src/history.c
+++ src/history.c
@@ -46,7 +46,7 @@ be called before any other history function is called.
 @param path             Directory where history files reside
 @return HS_SUCCESS
 */
-INT hs_set_path(char *path)
+INT hs_set_path(const char *path)
 {
    /* set path locally and remotely */
    if (rpc_is_remote())
@@ -280,7 +280,7 @@ INT hs_search_file(DWORD * ltime, INT direction)
 
 
 /********************************************************************/
-INT hs_define_event(DWORD event_id, char *name, TAG * tag, DWORD size)
+INT hs_define_event(DWORD event_id, const char *name, const TAG * tag, DWORD size)
 /********************************************************************\
 
   Routine: hs_define_event
@@ -370,12 +370,6 @@ INT hs_define_event(DWORD event_id, char *name, TAG * tag, DWORD size)
       rec.time = (DWORD) time(NULL);
       rec.data_size = size;
       strncpy(event_name, name, NAME_LENGTH);
-
-      /* pad tag names with zeos */
-      for (i = 0; (DWORD) i < size / sizeof(TAG); i++) {
-         len = strlen(tag[i].name);
-         memset(tag[i].name + len, 0, NAME_LENGTH - len);
-      }
 
       /* if history structure not set up, do so now */
       if (!_history[index].hist_fh) {
@@ -516,7 +510,7 @@ INT hs_define_event(DWORD event_id, char *name, TAG * tag, DWORD size)
 
 
 /********************************************************************/
-INT hs_write_event(DWORD event_id, void *data, DWORD size)
+INT hs_write_event(DWORD event_id, const void *data, DWORD size)
 /********************************************************************\
 
   Routine: hs_write_event
@@ -845,7 +839,7 @@ INT hs_count_events(DWORD ltime, DWORD * count)
 
 
 /********************************************************************/
-INT hs_get_event_id(DWORD ltime, char *name, DWORD * id)
+INT hs_get_event_id(DWORD ltime, const char *name, DWORD * id)
 /********************************************************************\
 
   Routine: hs_get_event_id
@@ -1100,7 +1094,7 @@ INT hs_enum_vars(DWORD ltime, DWORD event_id, char *var_name, DWORD * size, DWOR
 
 
 /********************************************************************/
-INT hs_get_var(DWORD ltime, DWORD event_id, char *var_name, DWORD * type, INT * n_data)
+INT hs_get_var(DWORD ltime, DWORD event_id, const char *var_name, DWORD * type, INT * n_data)
 /********************************************************************\
 
   Routine: hs_get_var
@@ -1293,7 +1287,7 @@ INT hs_get_tags(DWORD ltime, DWORD event_id, char event_name[NAME_LENGTH], int* 
 }
 
 
-INT hs_read(DWORD event_id, DWORD start_time, DWORD end_time, DWORD interval, char *tag_name, DWORD var_index, DWORD * time_buffer, DWORD * tbsize, void *data_buffer, DWORD * dbsize, DWORD * type, DWORD * n)
+INT hs_read(DWORD event_id, DWORD start_time, DWORD end_time, DWORD interval, const char *tag_name, DWORD var_index, DWORD * time_buffer, DWORD * tbsize, void *data_buffer, DWORD * dbsize, DWORD * type, DWORD * n)
 /********************************************************************\
 
   Routine: hs_read
@@ -1951,7 +1945,7 @@ INT hs_dump(DWORD event_id, DWORD start_time, DWORD end_time, DWORD interval, BO
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
 
 /********************************************************************/
-INT hs_fdump(char *file_name, DWORD id, BOOL binary_time)
+INT hs_fdump(const char *file_name, DWORD id, BOOL binary_time)
 /********************************************************************\
 
   Routine: hs_fdump
