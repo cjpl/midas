141651c12cc8858592498ee244a02a4a88808226
Stefan Ritt <stefan.ritt@psi.ch>
2013-08-16 11:41:00 +0200

    Use ODBtable for help page

--- src/mhttpd.cxx
+++ src/mhttpd.cxx
@@ -912,7 +912,7 @@ void show_help_page()
    show_header("Help", "", "./", 0);
    show_navigation_bar("Help");
 
-   rsprintf("<table class=\"subStatusTable\">\n");
+   rsprintf("<table class=\"ODBTable\">\n");
    rsprintf("  <tr>\n");
    rsprintf("    <td class=\"subStatusTitle\">MIDAS Help Page</td>\n");
    rsprintf("  </tr>\n");
@@ -934,7 +934,7 @@ void show_help_page()
    rsprintf("        </tr>\n");
 
    rsprintf("        <tr>\n");
-   rsprintf("          <td style=\"text-align:right;\"><hr />Version:</td>\n");
+   rsprintf("          <td style=\"text-align:right;\">Version:</td>\n");
    rsprintf("          <td style=\"text-align:left;\">%s</td>\n", cm_get_version());
    rsprintf("        </tr>\n");
    rsprintf("        <tr>\n");
@@ -946,13 +946,13 @@ void show_help_page()
    rsprintf("        </tr>\n");
    
    rsprintf("        <tr>\n");
-   rsprintf("          <td style=\"text-align:right;\"><hr />Experiment:</td>\n");
+   rsprintf("          <td style=\"text-align:right;\">Experiment:</td>\n");
    cm_get_experiment_name(str, sizeof(str));
    rsprintf("          <td style=\"text-align:left;\">%s</td>\n", str);
    rsprintf("        </tr>\n");
    
    rsprintf("        <tr>\n");
-   rsprintf("          <td style=\"text-align:right;\"><hr />MIDAS_EXPTAB:</td>\n");
+   rsprintf("          <td style=\"text-align:right;\">MIDAS_EXPTAB:</td>\n");
    s = getenv("MIDAS_EXPTAB");
    if (!s) s = "";
    strlcpy(str, s, sizeof(str));
