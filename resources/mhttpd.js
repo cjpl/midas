caf91542cd6dac8ff6bd4c37ee376844d5794baa
Stefan Ritt <stefan.ritt@psi.ch>
2014-02-19 16:59:05 +0100

    Removed getMouseXY, fixed copy/paste issue in ODB inline editor.

--- resources/mhttpd.js
+++ resources/mhttpd.js
@@ -16,33 +16,6 @@ var TID_STRUCT = 14;
 var TID_KEY = 15;
 var TID_LINK = 16;
 
-document.onmousemove = getMouseXY;
-
-function getMouseXY(e)
-{
-   try {
-      var x = e.pageX;
-      var y = e.pageY;
-      var p = 'abs: ' + x + '/' + y;
-      i = document.getElementById('refimg');
-      if (i == null)
-         return false;
-      document.body.style.cursor = 'crosshair';
-      x -= i.offsetLeft;
-      y -= i.offsetTop;
-      while (i = i.offsetParent) {
-         x -= i.offsetLeft;
-         y -= i.offsetTop;
-      }
-      p += '   rel: ' + x + '/' + y;
-      window.status = p;
-      return true;
-      }
-   catch (e) {
-      return false;
-   }
-}
-
 function XMLHttpRequestGeneric()
 {
    var request;
